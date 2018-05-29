8eaf4eaa2ea19a0d56a7924d66c32145c41268ec
goran <goranaviani@gmail.com>
2018-05-29 18:29:44 +0200

    js

--- python-challenging-programming-exercises/javascript-challenging-programming-exercises/script.js
+++ python-challenging-programming-exercises/javascript-challenging-programming-exercises/script.js
@@ -70,15 +70,94 @@ console.log(findAllNumbers(1,8))
 # Hints:
 # In case of input data being supplied to the question, it should be assumed to be a console input.
 */
-
+/*
 function integralNUmbers(number){
     var i;
-    var resultList = []
+    var resultDict = {};
     for (i = 1; i < number +1; i++){
-        resultList.push(i);
-        resultList.push(i*i);
+        resultDict[i] = (i*i);
+
     }
-    return resultList
+    return resultDict
 }
 
 console.log(integralNUmbers(8))
+*/
+/*
+# Question 4
+# Level 1
+# Write a program which accepts a sequence of comma-separated numbers from console and generate a list and a tuple which contains
+# every number. #Suppose the following input is supplied to the program:
+# 34,67,55,33,12,98
+# Then, the output should be:
+# ['34', '67', '55', '33', '12', '98']
+# ('34', '67', '55', '33', '12', '98')
+# Hints:
+# In case of input data being supplied to the question, it should be assumed to be a console input.
+# tuple() method can convert list to tuple
+*/
+/*
+function tupple(sequence) {
+    var sequencelist = [];
+    sequenceList = sequence.split(",");
+    var i;
+
+    console.log(sequenceList.length)
+    for (i = 0; i < 3; i++) {
+
+    }
+    return sequenceList
+}
+
+console.log(tupple("34,67,55,33,12,98"))
+*/
+
+/*# Question 5
+# Level 1
+# Question:
+# Define a class which has at least two methods: getString: to get a string from console input
+# printString: to print the string in upper case.
+# Also please include simple test function to test the class methods.
+
+
+*/
+
+/*
+# Question 6
+# Level 1
+#  Question:
+# Write a program that calculates and prints the value according to the given formula:
+# Q = Square root of [(2 * C * D)/H]
+# Following are the fixed values of C and H:
+# C is 50. H is 30.
+# D is the variable whose values should be input to your program in a comma-separated sequence.
+# Example
+# Let us assume the following comma separated input sequence is given to the program:
+# 100,150,180
+# The output of the program should be:
+# 18,22,24
+# Hints:
+# If the output received is in decimal form, it should be rounded off to its nearest value (for example, if the output received is 26.0,
+# it should be printed as 26)
+# In case of input data being supplied to the question, it should be assumed to be a console input.
+*/
+
+/*
+function formula(numberList){
+    var C = 50;
+    var H = 30;
+    var formulaList = [];
+    var i = 0;
+    var resultList = [];
+    formulaList = numberList.split(",")
+
+    for (i = 0; i<formulaList.length; i++){
+        resultList.push(Math.round(Math.sqrt([(2 * C * formulaList[i])/H])))
+    }
+
+    return resultList;
+}
+
+console.log(formula("100,150,180"))
+*/
+
