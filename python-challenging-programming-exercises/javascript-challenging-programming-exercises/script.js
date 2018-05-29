6496de75007c52c62b1fd6e3cd2a5d4f9577b120
goran <goranaviani@gmail.com>
2018-05-29 19:22:42 +0200

    js

--- python-challenging-programming-exercises/javascript-challenging-programming-exercises/script.js
+++ python-challenging-programming-exercises/javascript-challenging-programming-exercises/script.js
@@ -200,3 +200,25 @@ function array2D(numbers){
 
 console.log(array2D("3,5"))
 */
+/*
+# Question 8
+# Level 2
+# Question:
+# Write a program that accepts a comma separated sequence of words as input and prints the words in a comma-separated
+# sequence after sorting them alphabetically.
+# Suppose the following input is supplied to the program:
+# without,hello,bag,world
+# Then, the output should be:
+# bag,hello,without,world
+# Hints:
+# In case of input data being supplied to the question, it should be assumed to be a console input.
+*/
+/*
+function sorting(text){
+    text = text.split(",");
+    return text.sort()
+
+}
+
+console.log(sorting("without,hello,bag,world"))
+*/
