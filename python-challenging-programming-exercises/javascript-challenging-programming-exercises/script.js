9594d53abc4edb707e8e6e383e0d91d410ae1bcc
goran <goranaviani@gmail.com>
2018-06-08 13:50:54 +0200

    js10

--- python-challenging-programming-exercises/javascript-challenging-programming-exercises/script.js
+++ python-challenging-programming-exercises/javascript-challenging-programming-exercises/script.js
@@ -260,3 +260,39 @@ function inputConsole(event){
 }
 */
 
+/*
+# Question 10
+# Level 2
+# Question:
+# Write a program that accepts a sequence of whitespace separated words as input and prints the words after removing
+# all duplicate words and sorting them alphanumerically.
+# Suppose the following input is supplied to the program:
+# hello world and practice makes perfect and hello world again
+# Then, the output should be:
+# again and hello makes perfect practice world
+# Hints:
+# In case of input data being supplied to the question, it should be assumed to be a console input.
+# We use set container to remove duplicated data automatically and then use sorted() to sort the data
+*/
+
+/*
+function uniq(a) {
+   return Array.from(new Set(a));
+}
+
+function inputConsole(event){
+    event.preventDefault();
+    let inputUser = document.getElementById("userInfoData");
+    inputUser = inputUser.value
+    console.log(inputUser)
+    inputUser = inputUser.split(" ");
+    console.log(inputUser)
+    inputUser.sort();
+    console.log(uniq(inputUser))
+
+}
+
+*/
+
+
+
