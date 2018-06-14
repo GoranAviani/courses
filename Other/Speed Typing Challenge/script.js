f684aaa38848348ccc60c52f48013d4e090c2a01
goran <goranaviani@gmail.com>
2018-06-14 14:07:05 +0200

    timer is working

--- Other/Speed Typing Challenge/script.js
+++ Other/Speed Typing Challenge/script.js
@@ -4,15 +4,34 @@ const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
- let timer = 0;
+    //minutes, seconds, hundrens of seconds, thousends of seconds
+ let timer = [0,0,0,0];
+
 // Add leading zero to numbers 9 or below (purely for aesthetics):
 
 
 // Run a standard minute/second/hundredths timer:
 
 function runTimer(){
-    theTimer.innerHTML = timer ;
-    timer++;
+
+    let currentTime = timer[0]+":"+timer[1]+":"+timer[2]
+
+    theTimer.innerHTML = currentTime ;
+    timer[3]++;
+
+
+
+    //minutes dividing by 100 to get seconds, and dividing by 60 to get minutes
+    timer[0] = Math.floor((timer[3]/100/60))
+
+    // - timer[0] * 60 = every time it hits 60 seconds this value returns to zero
+    timer[1] = Math.floor((timer[3]/100) - (timer[0] * 60))
+
+    // - timer[1] * 100 = every time it hits 100 part of a second it goes to 0
+    //every time a minute reach a hundred so it does not count over
+    timer[2] = Math.floor((timer[3] - timer[1] * 100 ) - (timer[0] * 6000))
+
+
 
 }
 
