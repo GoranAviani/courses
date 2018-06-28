2b1a59688682d46496a8ef499c966614324a298d
goran <goranaviani@gmail.com>
2018-06-28 15:07:21 +0200

    unique characters

--- Jupyter Interactive Coding Challenges Notebook/Arrays and Strings/UniqueChars/unique_chars.py
+++ Jupyter Interactive Coding Challenges Notebook/Arrays and Strings/UniqueChars/unique_chars.py
@@ -7,10 +7,19 @@
 #'bar' -> True
 
 
+
 class UniqueChars(object):
 
     def has_unique_char(self, word):
         if word == None:
             return False
-        elif len(word) == 0:
+        
+        word1 = ''.join(sorted(set(word), key=word.index))
+
+        if len(word) == 0:
             return True
+        elif word1 == word:
+            print(word)
+            return True
+        else:
+            return False
