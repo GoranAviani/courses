5c712bddca04daf7e21f87214ebc1b2bc57121dc
goran <goranaviani@gmail.com>
2018-06-28 14:53:29 +0200

    new challendges

---
+++ Jupyter Interactive Coding Challenges Notebook/Arrays and Strings/UniqueChars/unique_chars.py
@@ -0,0 +1,13 @@
+#Implement an algorithm to determine if a string has all unique characters.
+
+#Test Cases
+#None -> False
+#'' -> True
+#'foo' -> False
+#'bar' -> True
+
+
+class UniqueChars(object):
+
+    def has_unique_char(self, word):
+        pass
