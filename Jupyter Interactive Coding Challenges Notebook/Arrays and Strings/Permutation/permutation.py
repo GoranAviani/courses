d2a423c54d52c5e5cf59da3311d9d30edf26aca7
goran <goranaviani@gmail.com>
2018-06-28 15:33:35 +0200

    ttd permutation

--- Jupyter Interactive Coding Challenges Notebook/Arrays and Strings/Permutation/permutation.py
+++ Jupyter Interactive Coding Challenges Notebook/Arrays and Strings/Permutation/permutation.py
@@ -12,5 +12,17 @@
 class Permutation(object):
 
 
-    def check_permutation(self):
-        pass
+    def check_permutation(self, word1, word2):
+
+
+        if (word1 is None or word2 is None ):
+            return False
+
+        elif (len(word1) == 0 or len(word2) == 0):
+            return False
+
+        elif (("".join(set(word1))) == ("".join(set(word2)))):
+            return True
+        
+        else:
+            return False
