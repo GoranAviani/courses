6076e43471ea68d1bb166467f48ba8e27d434ff8
goran <goranaviani@gmail.com>
2018-06-28 15:17:53 +0200

    ttd permutation

---
+++ Jupyter Interactive Coding Challenges Notebook/Arrays and Strings/Permutation/permutation.py
@@ -0,0 +1,16 @@
+# Determine if a string is a permutation of another string.
+
+
+#Test Cases
+#    One or more None inputs -> False
+#   One or more empty strings -> False
+#    'Nib', 'bin' -> False
+#    'act', 'cat' -> True
+#    'a ct', 'ca t' -> True
+
+
+class Permutation(object):
+
+
+    def check_permutation(self):
+        pass
