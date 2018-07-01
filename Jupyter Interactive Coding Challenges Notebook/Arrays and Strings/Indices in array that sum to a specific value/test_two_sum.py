b900b76fcba43599159abd6e1b6175b8fb5b495a
goran <goranaviani@gmail.com>
2018-07-01 10:34:07 +0200

    Given an array, find the two indices that sum to a specific value.

---
+++ Jupyter Interactive Coding Challenges Notebook/Arrays and Strings/Indices in array that sum to a specific value/test_two_sum.py
@@ -0,0 +1,19 @@
+#Problem: Given an array, find the two indices that sum to a specific value.
+#Test Cases
+#    None input -> TypeError
+#    [] -> ValueError
+#    [1, 3, 2, -7, 5], 7 -> [2, 4]
+
+import unittest
+import two_sum
+
+class SumStringsTestCase(unittest.TestCase):
+
+    def test_two_sum(self):
+        my_two_sum = two_sum.TwoSum()
+
+        self.assertRaises(ValueError, my_two_sum.calculate_two_sum, [], 0)
+        target = 7
+        nums = [1, 3, 2, -7, 5]
+        expected = [2, 4]
+        self.assertEqual(my_two_sum.calculate_two_sum(nums, target), expected)
---
+++ Jupyter Interactive Coding Challenges Notebook/Arrays and Strings/Indices in array that sum to a specific value/two_sum.py
@@ -0,0 +1,6 @@
+
+
+class TwoSum(object):
+
+    def calculate_two_sum(self, list1, result):
+        pass
