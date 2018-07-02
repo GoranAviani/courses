e5f281ba11e07afa488e8754f93b646a73ba3cae
goran <goranaviani@gmail.com>
2018-07-02 13:19:42 +0200

    ttd selection sort

---
+++ Jupyter Interactive Coding Challenges Notebook/Sorting/selectionsort.py
@@ -0,0 +1,23 @@
+#Problem: Implement selection sort
+#Constraints
+#    Is a naive solution sufficient (ie not stable, not based on a heap)?
+#        Yes
+#   Are duplicates allowed?
+#        Yes
+#    Can we assume the input is valid?
+#        No
+#    Can we assume this fits memory?
+#        Yes
+#Test Cases
+#    None -> Exception
+#    Empty input -> []
+#    One element -> [element]
+#    Two or more elements
+
+class SelectionSort(object):
+
+    def selection_sort(self):
+        pass
+
+
+
