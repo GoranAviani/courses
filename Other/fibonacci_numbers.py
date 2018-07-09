87796bf0810c0e5dfbaa5bb812c078ab4a645f06
goran <goranaviani@gmail.com>
2018-07-09 13:09:20 +0200

    fibonacci numbers

--- Other/fibonacci_numbers.py
+++ Other/fibonacci_numbers.py
@@ -17,3 +17,42 @@ def fibonacci(limit):
     return numbers
 
 print(fibonacci(100))
+
+
+def fibonacci2(limit):
+    numbers = []
+    current = 0
+    next = 1
+    while current < limit:
+        #current, next = next, next + current
+
+        current_old = current
+        current = next
+        next = current_old + next
+        numbers.append(current)
+        yield current
+
+
+for x in fibonacci2(150):
+    print(x, end=", ")
+
+
+print("\n")
+def fibonacci2(limit):
+    numbers = []
+    current = 0
+    next = 1
+    while True:
+        #current, next = next, next + current
+
+        current_old = current
+        current = next
+        next = current_old + next
+        numbers.append(current)
+        yield current
+
+
+for x in fibonacci2(150):
+    if x > 1000:
+        break
+    print(x, end=", ")
