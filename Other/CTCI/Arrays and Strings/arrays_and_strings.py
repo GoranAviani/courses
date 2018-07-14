b4e02d4f2b1392b85e112ca4a6b5ee14164a63aa
goran <goranaviani@gmail.com>
2018-07-14 18:53:46 +0200

    ctci

--- Other/CTCI/Arrays and Strings/arrays_and_strings.py
+++ Other/CTCI/Arrays and Strings/arrays_and_strings.py
@@ -2,9 +2,25 @@
 #cannot use additional data structures?
 #Hints: #44, #7 7 7, #732
 
+#def unique(word):
+#    for x in word:
+#        if word.count(x) > 1:
+#            return False
+#    return True
+
+
+#without count
 def unique(word):
+    result = []
     for x in word:
-        if word.count(x) > 1:
+        if x in result:
             return False
+        else:
+            result.append(x)
+
+    return True
+
+
+
 
 print(unique("aaabb"))
--- Other/CTCI/Arrays and Strings/test_arrays_and_strings.py
+++ Other/CTCI/Arrays and Strings/test_arrays_and_strings.py
@@ -6,4 +6,6 @@ class ArraysTestCase(unittest.TestCase):
     def test_unique(self):
         my_unique = arrays_and_strings.unique
         self.assertEqual(my_unique("aaabb"), False)
-        self.assertEqual(my_unique("abcde"), None)
+        self.assertEqual(my_unique("abcde"), True)
+        self.assertEqual(my_unique("abcde312"), True)
+        self.assertEqual(my_unique("abcde33"), False)
