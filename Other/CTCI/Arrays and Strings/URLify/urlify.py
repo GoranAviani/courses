ce301fe75bb207fc06ee54b80a341c6c30b250ed
goran <goranaviani@gmail.com>
2018-07-16 22:44:06 +0200

    ttd ctci urlify recurstion solution

--- Other/CTCI/Arrays and Strings/URLify/urlify.py
+++ Other/CTCI/Arrays and Strings/URLify/urlify.py
@@ -3,6 +3,29 @@
 #length of the string. (Note: If implementing in Java, please use a character array so that you can
 #perform this operation in place.)
 
+def blanks(word1, result):
+    #print(result)
+    for x in word1:
+        if x == " ":
+            result.append("%20")
+            word1 = word1[1:-2]
+            #print(result)
+            return blanks(word1, result)
+
+        else:
+            result.append(x)
+            word1 = word1[1:]
+            #print(result)
+
+
+    return result
+
 
 def do_urlify(word1):
-    pass
+
+    word1 = list(word1)
+    result = []
+    result = blanks(word1, result)
+    #print(result)
+    return "".join(result)
+print(do_urlify("test auto kuca    "))
