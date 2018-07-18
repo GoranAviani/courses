7aa7b82c80ac41b2a07acc7e33052df38a2a0ef4
goran <goranaviani@gmail.com>
2018-07-18 20:01:17 +0200

    php version

---
+++ python-challenging-programming-exercises/php-challenging-programming-exercises/factorial.php
@@ -0,0 +1,12 @@
+<?php
+/* Write your PHP code here */
+$userInput = "8";
+$result = 1;
+
+for ($a = 1; $a < $userInput +1 ; $a++) {
+	$result = $result * $a;
+		}    
+
+
+echo $result.'<br />';
+?>
