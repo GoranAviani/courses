e91dad70809a0fab88880cf4073f76392cb26fce
goran <goranaviani@gmail.com>
2018-07-19 14:00:16 +0200

    square

--- python-challenging-programming-exercises/php-challenging-programming-exercises/square_numbers.php
+++ python-challenging-programming-exercises/php-challenging-programming-exercises/square_numbers.php
@@ -1,4 +1,6 @@
-/* # Question 3
+<?php
+/*
+# Question 3
 # Level 1
 # With a given integral number n, write a program to generate a dictionary that contains (i, i*i) such that is an integral number
 #  between 1 and n (both included). and then the program should print the dictionary.
@@ -10,3 +12,17 @@
 # In case of input data being supplied to the question, it should be assumed to be a console input.
 */
 
+$userInput = "8";
+$result = array();
+
+for ($a = 1; $a < $userInput +1; $a++){
+    echo $a."<br/>";
+    echo $a * $a ."<br/>";
+    array_push($result,$a, $a * $a);
+}
+
+foreach ($result as $res){
+    echo $res;
+}
+
+?>
