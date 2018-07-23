28c945820ff2350cb95c5ce10f9495ff47995ee1
goran <goranaviani@gmail.com>
2018-07-23 10:05:40 +0200

    2darray

--- python-challenging-programming-exercises/php-challenging-programming-exercises/2D_array.php
+++ python-challenging-programming-exercises/php-challenging-programming-exercises/2D_array.php
@@ -1,3 +1,5 @@
+<?php
+/*
 # Question 7
 # Level 2
 # Question:
@@ -11,3 +13,40 @@
 # [[0, 0, 0, 0, 0], [0, 1, 2, 3, 4], [0, 2, 4, 6, 8]]
 # Hints:
 # Note: In case of input data being supplied to the question, it should be assumed to be a console input in a comma-separated form.
+*/
+
+
+$userInput = "3,5";
+
+function twoDArray($userInput){
+	$result = array();
+	$userInput = explode(',', $userInput);
+
+
+		for ($i=0; $i < $userInput[0]; $i++){
+			//echo $i;
+			$tempArray = array();
+			for ($j=0; $j < $userInput[1]; $j++){
+
+				array_push($tempArray,$i * $j);
+				//echo "<br/>brojevi su ".$i * $j;
+			}
+
+			foreach ($tempArray as $ta){
+			//echo "<br/>temp array je ".$ta;
+			}
+			array_push($result, $tempArray);
+		}
+
+	foreach ($result as $res){
+		echo "<br/>";
+		echo implode(",",$res);
+	}
+
+
+}
+
+twoDArray($userInput);
+?>
+
+
