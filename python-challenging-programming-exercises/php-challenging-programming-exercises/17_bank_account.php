0a56c4259b4a022219b27a12a09e4f2b38c678ce
goran <goranaviani@gmail.com>
2018-07-27 12:46:09 +0200

    reworked bank account

--- python-challenging-programming-exercises/php-challenging-programming-exercises/17_bank_account.php
+++ python-challenging-programming-exercises/php-challenging-programming-exercises/17_bank_account.php
@@ -20,9 +20,9 @@
 */
 
 //Since its PHP I will send commands as an argument in a array of strings.
-function banking ($userInput){
-	$balance = 0;
-	
+function banking ($userInput, $balance){
+	//$balance = 0;
+	//echo "entrance balance is ". $balance."<br/>";
 	/*
 	//just checking the function argumet
 	foreach ($userInput as $ui){
@@ -30,38 +30,66 @@ function banking ($userInput){
 	echo "<br/>";
 	}
 	*/
+
+	//echo count($userInput);
 	for ($i = 0; $i < count($userInput); $i ++){
-		//echo $userInput[$i];
+		//echo "current : ".$userInput[$i]."<br/>";
 		$ui = explode(" ",$userInput[$i]);
-		
+
+
 		// $j < 1 because input has only 2 parts, command and number
 		for ($j = 0; $j < 1; $j++){
-			echo "first part: ".$ui[0]."<br/>";
-			echo "second part: ".$ui[1]."<br/>";
-		
+			//echo "first part: ".$ui[0]."<br/>";
+			//echo "second part: ".$ui[1]."<br/>";
+
 			if ($ui[0] == "D"){
 				$balance += $ui[1];
 			}
-			
+
 			if ($ui[0] == "W"){
 				$balance -= $ui[1];
 			}
-		
-		
-			
+
+
+
 		}
-		
-	echo "Bank balance is: " .$balance ."<br/>";
-			
+
+
 	}
-	
-	
-	
-	
+
+return "<br/> Banks balance is: " .$balance ."<br/>";
+
+
+
 }
 
 
-$userInput = array("D 300", "D 300", "W 200", "D 100");
-banking($userInput);
+//$userInput = array("D 300", "D 300", "W 200", "D 100");
+//echo banking($userInput, 0);
+
+
+$result = 100;
+$userInput = array("D 300");
+$result = banking($userInput, $result);
+
+
+$result = 400;
+$userInput = array("D 300");
+$result = banking($userInput, $result);
+
+
+$result = 700;
+$userInput = array("W 200");
+$result = banking($userInput, $result);
+
+
+
+$result = 500;
+$userInput = array("D 100");
+$result = banking($userInput, $result);
+
+
+echo $result;
+
 
 ?>
