4e59d4209f299e113da9ae30e097619b775ea7fe
GoranAviani <goranaviani@gmail.com>
2018-08-21 11:49:54 +0200

    appending list to list and test

---
+++ Other/w3resource/array/a7_append_elements_of_list_to_list.py
@@ -0,0 +1,16 @@
+#7. Write a Python program to append items from inerrable to the end of the array.
+
+
+def addingToList(nums):
+    need_to_add = nums
+
+    nums.extend(need_to_add)
+    return nums
+
+
+
+array_num = [1, 3, 5, 7, 9]
+result = addingToList(array_num)
+
+for x in result:
+    print(x)
