891c6e45ebde822ad09becf6bc702718f99d0ea9
goran <goranaviani@gmail.com>
2018-08-21 19:43:26 +0200

    odd even nums

---
+++ Other/w3/Statements and loops/_5_reverse_word.py
@@ -0,0 +1,6 @@
+#5 Write a Python program that accepts a word from the user and reverse it.
+
+
+userInput = "Goran"
+
+print(userInput[::-1])
--- Other/w3/Statements and loops/_6_reverse_word.py
+++
@@ -1,6 +0,0 @@
-#6 Write a Python program that accepts a word from the user and reverse it.
-
-
-userInput = "Goran"
-
-print(userInput[::-1])
