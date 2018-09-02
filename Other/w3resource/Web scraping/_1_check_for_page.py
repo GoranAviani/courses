fe547ecb6585e2f903fe46efcb21c9143bd33455
GoranAviani <goranaviani@gmail.com>
2018-09-02 15:46:40 +0200

    check for page

---
+++ Other/w3resource/Web scraping/_1_check_for_page.py
@@ -0,0 +1,24 @@
+#1. Write a Python program to test if a given page is found or not on the server.
+
+
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from urllib.error import URLError
+
+
+while True:
+    userURL = input("enter a webpage without a https://: or x to E[x]it!: ")
+
+    if userURL.lower() == "x":
+        print("Good bye")
+        break
+
+    else:
+        try:
+            html = urlopen("https://" + userURL)
+        except HTTPError as e:
+            print("HTTP error")
+        except URLError as e:
+            print("Server not found")
+        else:
+            print("HTMP page: \n\n----------------------------------------------------------------------------\n\n")
