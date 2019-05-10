911742f8193356c25a1fab592d345aadf74ce4a2
goranaviani <goranaviani@gmail.com>
2019-05-10 16:48:38 +0200

    task completed

--- automate_borning_stuff_with_python/15 keeping time, scheduling tasks and launching programs/show_start_end_times.py
+++ automate_borning_stuff_with_python/15 keeping time, scheduling tasks and launching programs/show_start_end_times.py
@@ -1,8 +1,19 @@
 import time
 
-def main():
-    pass
 
+def calcWastedTime():
+    wastingTime = 1
+
+    for i in range(1, 100000):
+        wastingTime = wastingTime * i
+    return wastingTime
+
+def main():
+    startTime = time.time()
+    wastedTime = calcWastedTime()
+    endTime = time.time()
+
+    print("Start time was {}, end time was {}\n It took {} seconds to finish" . format(startTime,endTime, endTime - startTime))
 
 if __name__ == "__main__":
     main()
