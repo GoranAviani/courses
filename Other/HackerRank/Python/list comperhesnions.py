37d22f28ee27e0b58083578b4e4901cab20fa824
goranaviani <goranaviani@gmail.com>
2019-05-20 17:22:51 +0200

    try

---
+++ Other/HackerRank/Python/list comperhesnions.py
@@ -0,0 +1,7 @@
+x, y, z, n = int(input()), int(input()), int(input()), int(input())
+if x + y + z != n:
+    for x in range (0,x+1):
+        for y in range (0, y+1):
+            for z in range(0, z + 1):
+                if x + y + z != n:
+                    print("{} {} {}" .format(x,y,z))
