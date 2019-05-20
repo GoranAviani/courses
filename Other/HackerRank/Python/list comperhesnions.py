4bead69363d7a374e83db11807225eb8b0221100
goranaviani <goranaviani@gmail.com>
2019-05-20 17:27:08 +0200

    List Comprehensions - reformating the result to fit hacker rank format

--- Other/HackerRank/Python/list comperhesnions.py
+++ Other/HackerRank/Python/list comperhesnions.py
@@ -1,13 +1,16 @@
 
 def main():
+    result = []
     x, y, z, n = int(input()), int(input()), int(input()), int(input())
     if x + y + z != n:
         for x in range (0,x+1):
             for y in range (0, y+1):
                 for z in range(0, z + 1):
                     if x + y + z != n:
-                        print("{} {} {}" .format(x,y,z))
+                      #  print("{} {} {}" .format(x,y,z))
+                        result.append([x,y,z])
 
 
+    print(result)
 if __name__ == "__main__":
     main()
