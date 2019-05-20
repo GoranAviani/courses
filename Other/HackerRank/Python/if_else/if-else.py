4733196441fb02f10b40473fe178227fb59e4f82
goranaviani <goranaviani@gmail.com>
2019-05-20 13:47:26 +0200

    Simple check. task completed

--- Other/HackerRank/Python/if_else/if-else.py
+++ Other/HackerRank/Python/if_else/if-else.py
@@ -1,18 +1,30 @@
+def input_check(userInput):
+    if 1<= userInput <= 100:
+        pass
+    else:
+        print("This number is not in a approved range.")
+        return ("no")
 
 def main():
     userInput = input ("enter a number from 1 to 100: ")
 
+
     userInput = int(userInput)
 
-    if userInput % 2 == 0:
-        if 2 <= userInput <= 5:
-            print("not wierd")
-        elif 6 <= userInput <= 20:
-            print("weird")
-        elif userInput > 20:
-            print("not weird")
+    status = input_check(userInput)
+
+    if status == "no":
+        pass
     else:
-        print("wierd")
+        if userInput % 2 == 0:
+            if 2 <= userInput <= 5:
+                print("not wierd")
+            elif 6 <= userInput <= 20:
+                print("weird")
+            elif userInput > 20:
+                print("not weird")
+        else:
+            print("wierd")
 
 
 if __name__ == "__main__":
