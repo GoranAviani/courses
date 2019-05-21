4d6f4a3867a43a982af49c1c2c6be512860be1b6
goranaviani <goranaviani@gmail.com>
2019-05-21 17:03:01 +0200

    student dictiornary of lists

--- Other/HackerRank/Python/finding the percentage.py
+++ Other/HackerRank/Python/finding the percentage.py
@@ -7,15 +7,17 @@ def main():
     for x in range(n):
         userInput = input()
         userInput = userInput.split()
+        hasName = False
         for y in userInput:
-            hasName = False
+
             if hasName == False:
                 name = y
                 hasName = True
             else:
                 scores.append(y)
-        scores = []
         student_marks[name] = scores
+        scores = []
+
 
     print(student_marks)
     queryName = input()
