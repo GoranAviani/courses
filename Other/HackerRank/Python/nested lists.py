60817094a6b14380d33808f512917646c067d12c
goranaviani <goranaviani@gmail.com>
2019-05-22 21:22:03 +0200

    student with the smallest score

--- Other/HackerRank/Python/nested lists.py
+++ Other/HackerRank/Python/nested lists.py
@@ -9,8 +9,15 @@ def main():
 
     print(students)
 
+    smallestScore = students[0][1]
 
+    for student in students:
 
+        if smallestScore > student[1]:
+            smallestScore = student[1]
+            smallestName = student[0]
+
+    print("student {} has the smallest score of {} " .format(smallestName, smallestScore))
 
 if __name__ == '__main__':
     main()
