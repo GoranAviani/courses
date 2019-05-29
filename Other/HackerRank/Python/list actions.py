a28333f202d2c79d428f1c7d92a46e071eea6abd
goranaviani <goranaviani@gmail.com>
2019-05-29 17:36:51 +0200

    quickfix

--- Other/HackerRank/Python/list actions.py
+++ Other/HackerRank/Python/list actions.py
@@ -12,14 +12,14 @@ def main():
         userInput = str(input())
         commands = userInput.split(" ")
         if commands[0] == "insert":
-            result.insert(int(commands[1]), commands[2])
+            result.insert(int(commands[1]), int(commands[2]))
         elif commands[0] == "print":
             result = printing(result)
             print(result)
         elif commands[0] == "remove":
-            result.remove(commands[1])
+            result.remove(int(commands[1]))
         elif commands[0] == "append":
-            result.append(commands[1])
+            result.append(int(commands[1]))
         elif commands[0] == "sort":
             result = sorted(result)
         elif commands[0] == "pop":
