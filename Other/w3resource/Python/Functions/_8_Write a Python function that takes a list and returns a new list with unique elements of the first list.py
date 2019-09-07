160005754f1b829734998b9ec0d90d6ca785de06
goranaviani <goranaviani@gmail.com>
2019-09-07 16:38:16 +0200

    8

--- Other/w3resource/Python/Functions/_8_Write a Python function that takes a list and returns a new list with unique elements of the first list.py
+++ Other/w3resource/Python/Functions/_8_Write a Python function that takes a list and returns a new list with unique elements of the first list.py
@@ -3,5 +3,15 @@ def fun (list):
     for x in list:
        if x not in result:
            result.append(x)
-    
+
     return result
+
+
+if __name__ == '__main__':
+
+    assert fun(["k","u","c","a"]) == ["k","u","c","a"]
+    assert fun(["T","o"]) == ["T","o"]
+    assert fun([1,2,3,1,2,3]) == [1,2,3]
+
+
+    print("Coding complete!")
