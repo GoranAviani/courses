d8d10aef0e3bc780e2a6c257ca2e9ec55e2a59fb
goranaviani <goranaviani@gmail.com>
2019-10-17 17:41:40 +0200

    1. Write a Python class to convert an integer to a roman numeral

--- Other/w3resource/Python/class/_1__Write a Python class to convert an integer to a roman numeral.py
+++ Other/w3resource/Python/class/_1__Write a Python class to convert an integer to a roman numeral.py
@@ -6,4 +6,12 @@ class PySolution:
                      90: "XC", 50: "L", 40: "XL", 10: "X", 9: "IX", 5: "V", 4: "IV", 1: "I"}
         result = ""
 
+        for k, v in romanNums.items():
+            while number % k == 0 and number > 0:
+                number -= k
+                result += v
+        return result
 
+
+print(PySolution().calc_number(4000))
+print(PySolution().calc_number(1))
