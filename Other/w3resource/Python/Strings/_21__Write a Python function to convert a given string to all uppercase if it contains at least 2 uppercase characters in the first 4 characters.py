507e9bb1eb94fb4f55c6d799aee53a8da2bdf9f0
goranaviani <goranaviani@gmail.com>
2019-10-23 17:29:12 +0200

    21. Write a Python function to convert a given string to all uppercase if it contains at least 2 uppercase characters in the first 4 characters.

--- Other/w3resource/Python/Strings/_21__Write a Python function to convert a given string to all uppercase if it contains at least 2 uppercase characters in the first 4 characters.py
+++ Other/w3resource/Python/Strings/_21__Write a Python function to convert a given string to all uppercase if it contains at least 2 uppercase characters in the first 4 characters.py
@@ -1,1 +1,19 @@
-#21. Write a Python function to convert a given string to all uppercase if it contains at least 2 uppercase characters in the first 4 characters.
+#21. Write a Python function to convert a given string to all uppercase if it
+# contains at least 2 uppercase characters in the first 4 characters.
+
+def fun(text):
+    counter = 0
+    for x in text:
+        if x.isupper():
+            counter += 1
+            if counter >1:
+                return text.upper()
+    return text
+
+if __name__ == '__main__':
+    assert fun("SuMama") == "SUMAMA"
+    assert fun("Sumama") == "Sumama"
+    assert fun("SUMama") == "SUMAMA"
+    assert fun("sumamA") == "sumamA"
+
+    print('Testing completed!')
