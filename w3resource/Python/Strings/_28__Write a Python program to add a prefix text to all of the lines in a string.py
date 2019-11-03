6c57b593287ea371de99d59ca07f96159fce43b7
goranaviani <goranaviani@gmail.com>
2019-11-03 18:04:56 +0100

    #28. Write a Python program to add a prefix text to all of the lines in a string.

--- w3resource/Python/Strings/_28__Write a Python program to add a prefix text to all of the lines in a string.py
+++ w3resource/Python/Strings/_28__Write a Python program to add a prefix text to all of the lines in a string.py
@@ -1,34 +1,20 @@
 #28. Write a Python program to add a prefix text to all of the lines in a string.
 
 
-
 def fun (text, prefix):
-    addNewPrefix = True # we need to add a prefix on the start
     result = []
-    text = text.split()
-
+    text = text.split("\n")
+    print(text)
     for x in text:
-        if addNewPrefix == True:
-            result.append(prefix + " " + x)
-            addNewPrefix = False
-        else:
-            #TODO
-            if x == '\n':
-                addNewPrefix = True
-            else:
-                result.append(x + " ")
+        result.append(prefix + " " + x)
 
 
-    result = "".join(result)
+    result = "\n".join(result)
     return result
-
-
-
-
 
 
 if __name__ == '__main__':
     # These "asserts" are used for self-checking and not for testing
-    assert fun("sumama i gorama.\nsumama i gorama.\nsumama i gorama.", "po ") == "po sumama i gorama.\npo sumama i gorama.\npo sumama i gorama."
+    assert fun("sumama i gorama.\nsumama i gorama.\nsumama i gorama.", "po") == "po sumama i gorama.\npo sumama i gorama.\npo sumama i gorama."
 
     print('Testing completed!')
