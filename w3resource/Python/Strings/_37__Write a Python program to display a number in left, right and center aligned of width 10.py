9e36a858dd5d611eed2b471cfb54e88a28fa4bb8
goranaviani <goranaviani@gmail.com>
2019-11-05 17:06:09 +0100

    37. Write a Python program to display a number in left, right and center aligned of width 10.

---
+++ w3resource/Python/Strings/_37__Write a Python program to display a number in left, right and center aligned of width 10.py
@@ -0,0 +1,15 @@
+#37. Write a Python program to display a number in left, right and center aligned of width 10.
+
+
+
+def fun(number):
+    pass
+
+
+if __name__ == "__main__":
+    # These "asserts" are used for self-checking and not for testing
+    assert fun("22") == "22        "
+    assert fun("22") == "        22"
+    assert fun("22") == "    22    "
+
+    print('Testing completed!')
