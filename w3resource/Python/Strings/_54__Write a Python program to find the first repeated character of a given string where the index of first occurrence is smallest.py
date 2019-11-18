465e13ede2c1c81b2f69e788e5a0db544dda338a
goranaviani <goranaviani@gmail.com>
2019-11-18 20:20:02 +0100

    #54. Write a Python program to find the first repeated character # of a given string where the index of first occurrence is smallest
    # extra task: show all repeated characters occurances

--- w3resource/Python/Strings/_54__Write a Python program to find the first repeated character of a given string where the index of first occurrence is smallest.py
+++ w3resource/Python/Strings/_54__Write a Python program to find the first repeated character of a given string where the index of first occurrence is smallest.py
@@ -1,20 +1,30 @@
 #54. Write a Python program to find the first repeated character
 # of a given string where the index of first occurrence is smallest
 
+# extra task: show all repeated characters occurances
+
+
 
 # first found will always have the smallest index number.
-def fun(text):
+#def fun(text):
+#    allChars = list(text)
+##    for x in text:
+#         if allChars.count(x) > 1:
+#            return x
+
+def fun (text):
+    allOccurances ={}
     allChars = list(text)
-    for x in text:
-         if allChars.count(x) > 1:
-            return x
-
-
+    for x in range(0, len(allChars)):
+        if allChars.count(allChars[x]) > 2:
+             if allChars[x] not in allOccurances:
+                    allOccurances[allChars[x]] = x
+    return allOccurances
 
 if __name__ == "__main__":
     # These "asserts" are used for self-checking and not for testing
-    assert fun("aebcedei") == "a"
-    assert fun("aebeqerewrewrecaedei") == "a"
-    assert fun("ebqrwrwrcadi") == "w"
+    assert fun("aebcedei") == {'e': 1}
+    assert fun("aebeqerewrewrecaedei") == {'e': 1, 'r': 6}
+    assert fun("ebqrwrwrcadi") == {'r': 3}
 
     print('Testing completed!')
