45374a6dd597708a087df8f11e28cd5c5e48d238
goranaviani <goranaviani@gmail.com>
2019-11-19 19:27:45 +0100

    60

---
+++ w3resource/Python/Strings/_60__Write a Python program to capitalize first and last letters of each word of a given string.py
@@ -0,0 +1,19 @@
+# 60. Write a Python program to capitalize first and last letters of each word of a given string.
+
+def fun(text):
+    allWords = text.split(" ")
+    result = []
+
+    for x in allWords:
+        fixedWord = x[:1].upper() + x[1:len(x) - 1] + x[len(x) - 1: len(x)].upper()
+        result.append(fixedWord)
+
+    result = " ".join(result)
+    return result
+
+
+if __name__ == "__main__":
+    # These "asserts" are used for self-checking and not for testing
+    assert fun("auto") == ("AutO")
+   
+    print('Testing completed!')
