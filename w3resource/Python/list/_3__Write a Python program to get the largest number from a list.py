292c4328b2371532fc9be98e483f7ae91a2cc37e
goranaviani <goranaviani@gmail.com>
2019-11-25 19:53:29 +0100

    # 3. Write a Python program to get the largest number from a list.

--- w3resource/Python/list/_3__Write a Python program to get the largest number from a list.py
+++ w3resource/Python/list/_3__Write a Python program to get the largest number from a list.py
@@ -1,11 +1,14 @@
 # 3. Write a Python program to get the largest number from a list.
 def fun(numbers):
-    biggest = 0
-    for x in numbers:
-        if x > biggest:
-            biggest = x
+    # biggest = 0
+    # for x in numbers:
+    #   if x > biggest:
+    #     biggest = x
 
-    return biggest
+    # return biggest
+
+    numbers.sort()
+    return numbers.pop()
 
 
 if __name__ == "__main__":
@@ -13,6 +16,6 @@ if __name__ == "__main__":
     assert fun([1, 2, 3]) == 3
     assert fun([1, 2, -3]) == 2
     assert fun([12, -3]) == 12
-    assert fun([2, 3, 5, 1, 14, 22, 1, 6, 7, 8]) == 22
+    assert fun([2, 3, 5, 1, 14, 22, 1, 6, 7, 8, 11, 3, 4, 44, 55, 2, 1, 3, 5, 8, 7, 13, 25]) == 55
 
     print('Testing completed!')
