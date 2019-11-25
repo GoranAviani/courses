2c57f199b92ed545b49040f52e7ccb966edeca55
goranaviani <goranaviani@gmail.com>
2019-11-25 19:58:06 +0100

    7. Write a Python program to remove duplicates from a list.

--- w3resource/Python/list/_7__Write a Python program to remove duplicates from a list.py
+++ w3resource/Python/list/_7__Write a Python program to remove duplicates from a list.py
@@ -1,4 +1,5 @@
 # 7. Write a Python program to remove duplicates from a list.
+
 def fun(text):
     result = []
     for x in text:
@@ -13,5 +14,7 @@ def fun(text):
 if __name__ == "__main__":
     # These "asserts" are used for self-checking and not for testing
     assert fun(["a", "b", "a", "c", "a", "b"]) == ["a", "b", "c"]
+    assert fun(["a", "b", "a", "c", "a", "b", "a", "1", "bb"]) == ["a", "b", "c", "1", "bb"]
+    assert fun(["a", "b", "a", "c", "a", "b", "a", "1", "bb", 1, 2, 1]) == ["a", "b", "c", "1", "bb", 1, 2]
 
     print('Testing completed!')
