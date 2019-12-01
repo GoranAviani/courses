82ae88fd515fd46b30101b29761067dcad64c124
goranaviani <goranaviani@gmail.com>
2019-12-01 16:16:58 +0100

    # 26. Write a python program to check whether two lists are circularly identical

---
+++ w3resource/Python/list/_26__Write a python program to check whether two lists are circularly identical.py
@@ -0,0 +1,13 @@
+# 26. Write a python program to check whether two lists are circularly identical
+
+def fun(item1, item2):
+
+
+# find first occurance of lower number
+# and reset positions(but keep order)
+
+if __name__ == "__main__":
+    # These "asserts" are used for self-checking and not for testing
+    #assert fun([1, 2, 3, 0], [2, 3, 0, 1]) == True
+
+    print('Testing completed!')
