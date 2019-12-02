2f7afcae76bec9f15fcdf12ef6bad2de3b2b0f79
goranaviani <goranaviani@gmail.com>
2019-12-02 21:44:32 +0100

    # 26. Write a python program to check whether two lists are circularly identical

--- w3resource/Python/list/_26__Write a python program to check whether two lists are circularly identical.py
+++ w3resource/Python/list/_26__Write a python program to check whether two lists are circularly identical.py
@@ -1,13 +1,56 @@
-# 26. Write a python program to check whether two lists are circularly identical
+
+# 26. Write a python program to check whether two lists are circularly identical.
 
 def fun(item1, item2):
-    pass
 
-# find first occurance of lower number
-# and reset positions(but keep order)
+
+    # find the smallest number of list1
+    smallest = min(item1)
+    # set both lists to the smallest number and start looping and comparing
+    list1 = []
+    list2 = []
+
+    beforeSmallest = []
+    smallestFound = False
+    for x in item1:
+        if smallestFound == True:
+            list1.append(x)
+        if smallestFound == False:
+            beforeSmallest.append(x)
+            if x == smallest:
+                smallestFound = True
+
+
+    list1 = list1 + beforeSmallest
+
+
+    beforeSmallest = []
+    smallestFound = False
+    for x in item2:
+        if smallestFound == True:
+            list2.append(x)
+        if smallestFound == False:
+            beforeSmallest.append(x)
+            if x == smallest:
+                smallestFound = True
+
+
+    list2 = list2 + beforeSmallest
+
+    print(list1)
+    print(list2)
+
+    for x in range(0, len(list1)):
+        if list1[x] == list2[x]:
+            pass
+        else:
+            return False
+        return True
 
 if __name__ == "__main__":
     # These "asserts" are used for self-checking and not for testing
-    #assert fun([1, 2, 3, 0], [2, 3, 0, 1]) == True
+    assert fun([1, 2, 3, 0, 4], [3, 0, 4, 1, 2]) == True
+
+
 
     print('Testing completed!')
