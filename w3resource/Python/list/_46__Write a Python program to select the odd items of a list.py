180d676d980f7651e0c7c943a847843afd4081d3
goranaviani <goranaviani@gmail.com>
2019-12-17 18:49:43 +0100

    #46. Write a Python program to select the odd items of a list.

--- w3resource/Python/list/_46__Write a Python program to select the odd items of a list.py
+++ w3resource/Python/list/_46__Write a Python program to select the odd items of a list.py
@@ -1,8 +1,16 @@
 #46. Write a Python program to select the odd items of a list.
 
 def fun(item1):
-    result =[]
-    for x in item1:
+#    result =[]
+#    for x in item1:
+#        if x % 2 == 0:
+#            pass
+#        else:
+#            result.append(x)
+#    return result
+
+    result = []
+    for x in range(0, len(item1)):
         if x % 2 == 0:
             pass
         else:
@@ -13,4 +21,6 @@ def fun(item1):
 if __name__ == "__main__":
     # These "asserts" are used for self-checking and not for testing
     assert fun([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) == [1, 3, 5, 7, 9]
+
+
     print('Testing completed!')
