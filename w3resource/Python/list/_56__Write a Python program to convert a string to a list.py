b4ebd9a11d93ce5066049e1208a4004c2242b6e7
goranaviani <goranaviani@gmail.com>
2019-12-29 16:47:17 +0100

    #56. Write a Python program to convert a string to a list

--- w3resource/Python/list/_56__Write a Python program to convert a string to a list.py
+++ w3resource/Python/list/_56__Write a Python program to convert a string to a list.py
@@ -4,13 +4,14 @@ def fun(item1, item2):
    # return item1.split(" ")
 
 # convert a string with a user inputed delimeter
-    result = []
-    for x in item1:
-        if x != item2:
-            result.append(x)
+  #  result = []
+  #  for x in item1:
+  #      if x != item2:
+  #          result.append(x)
 
-    return result
+  #  return result
 
+    return item1.split(item2)
 
 if __name__ == "__main__":
     # These "asserts" are used for self-checking and not for testing
