5fb28b6de64bc07a317e514d8685e5b4b9ec03f8
goranaviani <goranaviani@gmail.com>
2020-01-01 17:46:24 +0100

    # _62__Write a Python program to print a list of space-separated elements

--- w3resource/Python/list/_62__Write a Python program to print a list of space-separated elements.py
+++ w3resource/Python/list/_62__Write a Python program to print a list of space-separated elements.py
@@ -1,18 +1,20 @@
-# _62__Write a Python program to print a list of space-separated elements
+#_62__Write a Python program to print a list of space-separated elements
 
 def fun(item1):
-    result = ""
-    for x in range(0, len(item1)):
-        if x == len(item1) - 1:
-            result += str(item1[x])
-        else:
-            result += str(item1[x]) + " "
+#    result = ""
+#    for x in range(0, len(item1)):
+#       if x == len(item1)-1:
+#            result += str(item1[x])
+#       else:
+#            result += str(item1[x]) + " "
 
-    return result
+#    return result
 
+    return " ".join(item1)
 
 if __name__ == "__main__":
     # These "asserts" are used for self-checking and not for testing
-    assert fun([1, 3, 5, 7, 9, 10]) == "1 3 5 7 9 10"
+    assert fun(["a", "b", "5", "7", "a"]) == "a b 5 7 a"
+
 
     print('Testing completed!')
