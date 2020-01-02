23e928391afafd24f898730529e53db2efc8b89f
goranaviani <goranaviani@gmail.com>
2020-01-02 21:02:17 +0100

    # 67. Write a Python program to find all the values in a list are greater than a specified number.

--- w3resource/Python/list/_67__Write a Python program to find all the values in a list are greater than a specified number.py
+++ w3resource/Python/list/_67__Write a Python program to find all the values in a list are greater than a specified number.py
@@ -1,6 +1,21 @@
 # 67. Write a Python program to find all the values in a list are greater than a specified number.
 
 
+# def fun(**kwargs):
+#  try:
+#    item1 = kwargs["item1"]
+#    item2 = kwargs["item2"]
+#  except:
+#    return "Item 1 is empty."
+
+#  result = []
+#  for x in item1:
+#    if x > item2:
+#      result.append(x)
+
+#  return result
+
+
 def fun(**kwargs):
     try:
         item1 = kwargs["item1"]
@@ -8,11 +23,14 @@ def fun(**kwargs):
     except:
         return "Item 1 is empty."
 
-    result = []
-    for x in item1:
-        if x > item2:
-            result.append(x)
+    item1.sort()
+    index = 0
+    for x in range(0, len(item1)):
+        index += 1
+        if item1[x] > item2:
+            break
 
+    result = item1[x:]
     return result
 
 
