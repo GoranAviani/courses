722b2da62832181624a6ab0023f7ebd438af02b3
goranaviani <goranaviani@gmail.com>
2020-01-03 18:10:37 +0100

    # _82. Write a Python program to wrap a given string into a paragraph of given width.

--- w3resource/Python/Strings/_82__Write a Python program to wrap a given string into a paragraph of given width.py
+++ w3resource/Python/Strings/_82__Write a Python program to wrap a given string into a paragraph of given width.py
@@ -7,35 +7,55 @@ def fun(**kwargs):
     except:
         return "Item 1 or 2 are empty."
 
+    #counter = 0
+    #item1 = item1.split(" ")
+    #resultList = []
+    #result = ""
+    #for x in item1:
+    #    if (len(x) + counter <= item2):
+    #        result += x + " "
+    #        counter += len(x)
+    #    else:
+    #        resultList.append(result)
+    #        result = ""
+    #        counter = 0
+    #        result += x + " "
+    #        counter += 1
+    #if counter > 0:
+    #    resultList.append(result)
+    #    result = ""
+
+
+    #proccessedResult = ""
+    #for x in resultList:
+    #    proccessedResult += x + "\n"
+
+    #return proccessedResult
+
     counter = 0
     item1 = item1.split(" ")
     resultList = []
-    result = ""
+    result = []
     for x in item1:
         if (len(x) + counter <= item2):
-            result += x + " "
+            result.append(x)
             counter += len(x)
         else:
-            resultList.append(result)
-            result = ""
+            resultList.append(" ".join(result))
+            result = []
             counter = 0
-            result += x + " "
+            result.append(x)
             counter += 1
     if counter > 0:
-        resultList.append(result)
-        result = ""
+        resultList.append(" ".join(result))
+    joinedResult = "\n".join(resultList)
+    return joinedResult
 
-
-    proccessedResult = ""
-    for x in resultList:
-        proccessedResult += x + "\n"
-
-    return proccessedResult
 
 if __name__ == "__main__":
     # These "asserts" are used for self-checking and not for testing
-    itemOne = {"item1": "The quick brown fox"}
+    itemOne = {"item1": "The quick brown fox."}
     itemTwo = {"item2": 10}
-    assert fun(**itemOne, **itemTwo) == "The quick \nbrown fox \n"
+    assert fun(**itemOne, **itemTwo) == "The quick\nbrown fox."
 
     print('Testing completed!')
