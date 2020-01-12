81ae7c4ba3629747f4f3845db4a116b00d979b7c
goranaviani <goranaviani@gmail.com>
2020-01-12 14:04:14 +0100

    #34. Write a Python program to count number of items in a dictionary value that is a list.

--- w3resource/Python/dicts/_34__Write a Python program to count number of items in a dictionary value that is a list.py
+++ w3resource/Python/dicts/_34__Write a Python program to count number of items in a dictionary value that is a list.py
@@ -3,16 +3,24 @@
 
 
 def fun(item1):
+#    result = 0
+#    for k, v in item1.items():
+#        for x in v:
+#            result += 1
+#    return result
+
+#assuming we dont know iv values are lists
+#string way
     result = 0
     for k, v in item1.items():
-        for x in v:
-            result += 1
-
+        if str(v)[0:1] == "[":
+            for x in v:
+                result += 1
 
     return result
-
 if __name__ == "__main__":
     # These "asserts" are used for self-checking and not for testing
     assert fun({'Alex': ['subj1', 'subj2', 'subj3'], 'David': ['subj1', 'subj2']}) == 5
+    assert fun({'Alex': ['subj1', 'subj2', 'subj3'], 'David': 1}) == 3
 
     print('Testing completed!')
