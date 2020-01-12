c6bf7fc47fc5c6f8c6d30b32172cc2c8dc4a0367
goranaviani <goranaviani@gmail.com>
2020-01-12 13:49:18 +0100

    #33. Write a Python program to check multiple keys exists in a dictionary.

---
+++ w3resource/Python/dicts/_33__Write a Python program to check multiple keys exists in a dictionary.py
@@ -0,0 +1,18 @@
+#33. Write a Python program to check multiple keys exists in a dictionary.
+
+
+def fun(item1, item2):
+
+    for x in item2:
+        if x in item1:
+            pass
+        else:
+            return False
+
+
+    return True
+if __name__ == "__main__":
+    # These "asserts" are used for self-checking and not for testing
+    assert fun({11: 10, 12: 20, 23: 30, 34: 40, 15: 50, 16: 60}, [11, 34]) == True
+
+    print('Testing completed!')
