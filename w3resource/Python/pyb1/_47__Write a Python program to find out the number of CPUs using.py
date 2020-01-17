66948da6e14fb009a96916ba4caf02d8c13aade6
goranaviani <goranaviani@gmail.com>
2020-01-17 18:55:31 +0100

    #47. Write a Python program to find out the number of CPUs using

--- w3resource/Python/pyb1/_47__Write a Python program to find out the number of CPUs using.py
+++ w3resource/Python/pyb1/_47__Write a Python program to find out the number of CPUs using.py
@@ -1,5 +1,11 @@
 #47. Write a Python program to find out the number of CPUs using
-
-
 import multiprocessing
-print(multiprocessing.cpu_count())
+def fun():
+
+
+    return multiprocessing.cpu_count()
+if __name__ == "__main__":
+    # These "asserts" are used for self-checking and not for testing
+    fun() == 4
+    print('Testing completed!')
+
