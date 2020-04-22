cfa3a9d80800ca4d05bbb07cc797c065ce80e377
goranaviani <goranaviani@gmail.com>
2020-04-22 20:28:16 +0200

    Fixup on _6

--- w3resource/Python/pyb1/_6_comma_separated_numbers_into.py
+++ w3resource/Python/pyb1/_6_comma_separated_numbers_into.py
@@ -1,8 +1,8 @@
-#6. Write a Python program which accepts a sequence of comma-separated numbers from user and generate a list and a tuple with those numbers. Go to the editor
-#Sample data : 3, 5, 7, 23
-#Output :
-#List : ['3', ' 5', ' 7', ' 23']
-#Tuple : ('3', ' 5', ' 7', ' 23')
+# 6. Write a Python program which accepts a sequence of comma-separated numbers from user and generate a list and a tuple with those numbers. Go to the editor
+# Sample data : 3, 5, 7, 23
+# Output :
+# List : ['3', ' 5', ' 7', ' 23']
+# Tuple : ('3', ' 5', ' 7', ' 23')
 
 
 userInput = "3, 5, 7, 23"
@@ -11,15 +11,9 @@ resultList = []
 resultTuple = ()
 
 for x in listUserInput:
-   # print(x)
     resultList.append(x)
     resultTuple += (x,)
 
-   # print(type(resultList))
-   # print(type(resultTuple))
+for x in resultList: print(x)
 
-for x in resultList:
-   print(x)
-
-for z in resultTuple:
-    print(z)
+for z in resultTuple: print(z)
