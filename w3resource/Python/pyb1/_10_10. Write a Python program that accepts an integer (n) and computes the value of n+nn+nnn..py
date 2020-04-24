68c7f4b364fbc89ac7d27f94e0f68aca22aae6c4
goranaviani <goranaviani@gmail.com>
2020-04-24 20:29:17 +0200

    Fixup on _10

--- w3resource/Python/pyb1/_10_10. Write a Python program that accepts an integer (n) and computes the value of n+nn+nnn..py
+++ w3resource/Python/pyb1/_10_10. Write a Python program that accepts an integer (n) and computes the value of n+nn+nnn..py
@@ -7,10 +7,21 @@ Expected Result : 615
 def first(n):
     return int(n) + int(n + n) + int(n + n + n)
 
+def second(n):
+    number_string = ""
+    result = 0
+    n = int(n)
+
+    for x in range(0, 3):
+         number_string += str(n)
+         result += int(number_string)
+    return result
+
 def main():
     userInput = input("Enter a value: ")
-    result = first(userInput)
+    result, result1 = first(userInput), second(userInput)
     print(result)
+    print(result1)
 
 
 if __name__ == "__main__":
