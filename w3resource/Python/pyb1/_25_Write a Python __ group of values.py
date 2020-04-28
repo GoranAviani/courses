96378ffb50d32645bee4ca9b07624f43bafee679
goranaviani <goranaviani@gmail.com>
2020-04-28 19:09:14 +0200

    Fixup on _25

--- w3resource/Python/pyb1/_25_Write a Python __ group of values.py
+++ w3resource/Python/pyb1/_25_Write a Python __ group of values.py
@@ -1,10 +1,14 @@
-#25. Write a Python program to check whether a specified value is contained in a
-# group of values. Go to the editor
-#Test Data :
-#3 -> [1, 5, 8, 3] : True
-#-1 -> [1, 5, 8, 3] : False
+
 
 def main():
+    """
+    #25. Write a Python program to check whether a specified value is contained in a
+    # group of values. Go to the editor
+    #Test Data :
+    #3 -> [1, 5, 8, 3] : True
+    #-1 -> [1, 5, 8, 3] : False
+        :return: 
+    """
     groupOfValues = []
     for x in range(0, 4):
         userInput = input("Enter a number for a group of values")
