1d1bece95a4c54218ac9a13cb534d8639606c17f
goranaviani <goranaviani@gmail.com>
2020-04-29 16:58:53 +0200

    Fixup on _28

--- w3resource/Python/pyb1/_28_Write a Python program to print all even numbers from a given numbers list in the same order and stop the printing if any numbers that come after 237 in the sequence.py
+++ w3resource/Python/pyb1/_28_Write a Python program to print all even numbers from a given numbers list in the same order and stop the printing if any numbers that come after 237 in the sequence.py
@@ -1,4 +1,5 @@
-#28. Write a Python program to print all even numbers from a given numbers list in the same order and stop the printing if any numbers that come after 237 in the sequence. Go to the editor
+#28. Write a Python program to print all even numbers from a given numbers list in the same order and stop
+# the printing if any numbers that come after 237 in the sequence. Go to the editor
 #Sample numbers list :
 
 '''
