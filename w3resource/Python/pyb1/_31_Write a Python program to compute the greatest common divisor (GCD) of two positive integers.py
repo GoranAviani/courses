6ff4ffb6c1dbaf9f3f1a3d751597c00286a8b98a
goranaviani <goranaviani@gmail.com>
2020-04-30 15:53:16 +0200

    Fixup on _31

--- w3resource/Python/pyb1/_31_Write a Python program to compute the greatest common divisor (GCD) of two positive integers.py
+++ w3resource/Python/pyb1/_31_Write a Python program to compute the greatest common divisor (GCD) of two positive integers.py
@@ -1,13 +1,32 @@
-#31. Write a Python program to compute the greatest common divisor (GCD) of two positive integers.
+#31. Write a Python program to compute the greatest
+# common divisor (GCD) of two positive integers.
+
+def get_divisors(number_divisor, number_to_divide):
+  all_divisors = []
+  for divisor in range(2, number_divisor):
+    if number_to_divide % divisor == 0:
+      all_divisors.append(divisor)
+  return all_divisors
 
 
 def main():
-  userInput = input("Enter 2 numbers separated by comma (,) ")
-  userInput = userInput.split(",")
+  common_dividors = []
+  greatest_divisor = 0
+  user_input = input("Enter 2 numbers separated by comma (,) ")
+  user_input = user_input.split(",")
+  number_one = user_input[0]
+  number_two = user_input[1]
+  number_one = int(number_one)
+  number_two = int(number_two)
+  divisors_for_number_1 = get_divisors(number_one, number_two)
+  divisors_for_number_2 = get_divisors(number_two, number_one)
 
-  if (int(userInput[0]) % int(userInput[1]) == 0):
-    print(str(userInput[1]))
+  for x in divisors_for_number_1:
+    if x in divisors_for_number_2:
+      common_dividors.append(x)
 
+  greatest_divisor = max(common_dividors)
+  print(greatest_divisor)
   
 
 
