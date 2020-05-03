837e9e96474cebf6102cbd2b78f2191f2cc47d34
goranaviani <goranaviani@gmail.com>
2020-05-03 18:05:21 +0200

    Fixup on _33

--- w3resource/Python/pyb1/_33_Write a Python program to sum of three given integers. However, if two values are equal sum will be zero.py
+++ w3resource/Python/pyb1/_33_Write a Python program to sum of three given integers. However, if two values are equal sum will be zero.py
@@ -1,20 +1,19 @@
-
+#_33_Write a Python program to sum of three given integers.
+# However, if two values are equal sum will be zero
 def summa(x, y, z):
-
     allNums = [x, y, z]
-
     for n in allNums:
-        if allNums.count(n)> 1:
+        if allNums.count(n) > 1:
             return 0
     return x + y + z
 
-
-if __name__ == '__main__':
-    #print('Example:')
-    #print(checkio("Hello World hello"))
-
+def main():
     assert summa(3, 2, 1) == 6
     assert summa(2, 2, 2) == 0
     assert summa(2, 2, 1) == 0
     assert summa(2, 5, -1) == 6
     print("Coding complete!")
+
+
+if __name__ == '__main__':
+    main()
