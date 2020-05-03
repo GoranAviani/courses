d74a60f4519d7e60f16adc24d22d8c76aad26f02
goranaviani <goranaviani@gmail.com>
2020-05-03 20:10:03 +0200

    Fixup on _34

--- w3resource/Python/pyb1/_34_ Write a Python program to sum of two given integers. However, if the sum is between 15 to 20 it will return 20.py
+++ w3resource/Python/pyb1/_34_ Write a Python program to sum of two given integers. However, if the sum is between 15 to 20 it will return 20.py
@@ -1,4 +1,5 @@
-
+# _34_ Write a Python program to sum of two given integers.
+# However, if the sum is between 15 to 20 it will return 20
 
 def summa(x,y):
     if (((x+y)>14) and ((x+y) < 21)):
@@ -6,11 +7,7 @@ def summa(x,y):
     else:
         return (x+ y)
 
-
 if __name__ == '__main__':
-    #print('Example:')
-    #print(checkio("Hello World hello"))
-
     assert summa(11, 6) == 20
     assert summa(3, 2) == 5
     assert summa(10, 5) == 20
