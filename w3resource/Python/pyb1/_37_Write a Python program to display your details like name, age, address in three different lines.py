ac4a6715567741eacf42200745a8b7b4dd965086
goranaviani <goranaviani@gmail.com>
2020-05-05 20:37:43 +0200

    Fixup on _37

--- w3resource/Python/pyb1/_37_Write a Python program to display your details like name, age, address in three different lines.py
+++ w3resource/Python/pyb1/_37_Write a Python program to display your details like name, age, address in three different lines.py
@@ -4,9 +4,10 @@ def fun(name, age, address):
     result = name + "\n" + age + "\n" + address
     return result
 
-
-if __name__ == '__main__':
-
+def main():
     # These "asserts" are used for self-checking and not for testing
     assert fun('Goran', '32', 'Stockholm') == 'Goran\n32\nStockholm'
     print('Testing completed!')
+    
+if __name__ == '__main__':
+    main()
