3f6392ef55c893dbfefe896effcc2d0de7743002
goranaviani <goranaviani@gmail.com>
2020-05-06 13:28:23 +0200

    Fixup on _40

--- w3resource/Python/pyb1/_40__Write a Python program to compute the distance between the points (x1, y1) and (x2, y2)..py
+++ w3resource/Python/pyb1/_40__Write a Python program to compute the distance between the points (x1, y1) and (x2, y2)..py
@@ -1,12 +1,20 @@
-#40. Write a Python program to compute the distance between the points (x1, y1) and (x2, y2).
-def fun(item1, item2):
-    import math
-    dist = math.sqrt((item1[1] - item1[0])**2 + (item2[1] - item2[0])**2)
-    dist = round(dist, 2)
-    return dist
+#40. Write a Python program to compute the distance between the
+# points (x1, y1) and (x2, y2).
+import math
+
+def calculation(**kwargs):
+    try:
+        item1 = kwargs["item1"]
+        item2 = kwargs["item2"]
+    except KeyError as e:
+        print("Can not fetch variable {}" .format(e))
+    else:
+        dist = math.sqrt((item1[1] - item1[0])**2 + (item2[1] - item2[0])**2)
+        dist = round(dist, 2)
+        return dist
 
 if __name__ == "__main__":
     # These "asserts" are used for self-checking and not for testing
-    assert fun([4, 0], [6, 0]) == 7.21
-
+    all_items = {"item1": [4, 0], "item2": [6, 0]}
+    assert calculation(**all_items) == 7.21
     print('Testing completed!')
