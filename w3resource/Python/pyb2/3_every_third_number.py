d5709f297bcfd5c02b046c44d6bc9ef0597ae1eb
goranaviani <goranaviani@gmail.com>
2020-05-07 15:56:33 +0200

    Fixup on _3

--- w3resource/Python/pyb2/3_every_third_number.py
+++ w3resource/Python/pyb2/3_every_third_number.py
@@ -1,19 +1,17 @@
 #3. Write a Python program to remove and print every
 # third number from a list of numbers until the list becomes empty.
 
-def everyThird(userInput):
+def print_every_third(numbers):
  counter = 0
-
- for x in userInput:
+ for number in numbers:
      counter += 1
      if counter % 3 == 0:
-         print(x)
+         print(number)
 
 def main():
-    while True:
-        userInput = input("Enter a list of numbers separated bu comma: ")
-        userInput = userInput.split(',')
-        everyThird(userInput)
+    user_input = input("Enter a list of numbers separated bu comma: ")
+    user_input = user_input.split(',')
+    print_every_third(user_input)
 
 
 if __name__ == "__main__":
