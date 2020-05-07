dba89f98ee15eb442a46acff8e1a7a38542031ee
goranaviani <goranaviani@gmail.com>
2020-05-07 15:44:29 +0200

    Fixup on _42

--- w3resource/Python/pyb1/_42__Write a Python program to determine if a Python shell is executing in 32bit or 64bit mode on OS.py
+++ w3resource/Python/pyb1/_42__Write a Python program to determine if a Python shell is executing in 32bit or 64bit mode on OS.py
@@ -1,13 +1,17 @@
-#42. Write a Python program to determine if a Python shell is executing in 32bit or 64bit mode on OS.
+#42. Write a Python program to determine if a Python shell is
+# executing in 32bit or 64bit mode on OS.
+import struct
 
 def fun():
-    import struct
     result = struct.calcsize("P") * 8
     return result
 
-if __name__ == "__main__":
+def main():
     # These "asserts" are used for self-checking and not for testing
     assert fun() == 64
+
+if __name__ == "__main__":
+    main()
 
 
     print('Testing completed!')
