4d82f185ce1ee745994ebf702b265f76c912dc38
goranaviani <goranaviani@gmail.com>
2020-06-04 07:54:34 +0200

    sum_numbers

---
+++ Other/checkio/sum_numbers.py
@@ -0,0 +1,41 @@
+"""
+ In a given text you need to sum the numbers.
+ Only separated numbers should be counted.
+  If a number is part of a word it shouldn't be counted.
+
+The text consists from numbers, spaces and english letters
+
+Input: A string.
+
+Output: An int.
+
+ Example:
+sum_numbers('hi') == 0
+sum_numbers('who is 1st here') == 0
+sum_numbers('my numbers is 2') == 2
+sum_numbers('This picture is an oil on canvas '
+ 'painting by Danish artist Anna '
+ 'Petersen between 1845 and 1910 year') == 3755
+sum_numbers('5 plus 6 is') == 11
+sum_numbers('') == 0
+
+"""
+def sum_numbers(text):
+    result = 0
+    text = text.split(" ")
+    for word in text:
+        if word.isnumeric():
+            result += int(word)
+    return result
+
+def main():
+    assert sum_numbers('hi') == 0
+    assert sum_numbers('hi') == 0
+    assert sum_numbers('hi') == 0
+    assert sum_numbers('hi') == 0
+    assert sum_numbers('hi') == 0
+    print('Testing completed!')
+
+
+if __name__ == "__main__":
+    main()
