13b4e056a25933ae013b00d4c12c8d8daaee3d49
goranaviani <goranaviani@gmail.com>
2020-06-04 07:57:55 +0200

    sum_numbers

--- Other/checkio/sum_numbers.py
+++ Other/checkio/sum_numbers.py
@@ -30,10 +30,13 @@ def sum_numbers(text):
 
 def main():
     assert sum_numbers('hi') == 0
-    assert sum_numbers('hi') == 0
-    assert sum_numbers('hi') == 0
-    assert sum_numbers('hi') == 0
-    assert sum_numbers('hi') == 0
+    assert sum_numbers('who is 1st here') == 0
+    assert sum_numbers('my numbers is 2') == 2
+    assert sum_numbers('This picture is an oil on canvas '
+ 'painting by Danish artist Anna '
+ 'Petersen between 1845 and 1910 year') == 3755
+    assert sum_numbers('5 plus 6 is') == 11
+    assert sum_numbers('') == 0
     print('Testing completed!')
 
 
