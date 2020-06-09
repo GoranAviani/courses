0b4c3d352e0431342b53dd32109b770ac39ae3be
goranaviani <goranaviani@gmail.com>
2020-06-09 18:51:26 +0200

    user pc data

--- Other/get_user_pc_data.py
+++ Other/get_user_pc_data.py
@@ -6,3 +6,8 @@ from urllib.request import urlopen
 # get the username of machine
 username = getpass.getuser()
 print("\nUSERNAME   :   ", username)
+
+# get the hostname of machine
+hostname = socket.gethostname()
+print("\nHOSTNAME   :   ", hostname)
+# get the ip address of machine
