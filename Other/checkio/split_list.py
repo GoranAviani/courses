85da9690d42a6ca1023a9267136d16e1b63f9993
goranaviani <goranaviani@gmail.com>
2020-06-10 07:23:19 +0200

    split list - even number of elements. using floor division

--- Other/checkio/split_list.py
+++ Other/checkio/split_list.py
@@ -19,12 +19,12 @@ def split_list(list_to_split):
     element_list = []
     number_of_elements = len(list_to_split)
     if number_of_elements % 2 == 0:
-        split_number = number_of_elements / 2
-        for x in range(0, int(split_number)):
+        split_number = number_of_elements // 2
+        for x in range(0, split_number):
             element_list.append(list_to_split[x])
         result.append(element_list)
         element_list = []
-        for x in range(int(split_number), number_of_elements):
+        for x in range(split_number, number_of_elements):
             element_list.append(list_to_split[x])
         result.append(element_list)
 
