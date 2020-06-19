b816b24b361ee2edec29ae48c3d1044a254a4b29
goranaviani <goranaviani@gmail.com>
2020-06-19 09:23:50 +0200

    exercise_1_5.py

--- practical_python/part_1/exercise_1_5.py
+++ practical_python/part_1/exercise_1_5.py
@@ -1,11 +1,11 @@
 def main():
-    starting_height = 100
-    next_heights = []
-    for x in range(10):
-        starting_height = starting_height * 3 / 5
-        next_heights.append(starting_height)
-
-    for result in next_heights:
-        print(result)
+    height = 100
+    #next_heights = []
+    for x in range(1, 11):
+        height = height * 3 / 5
+        print("{}, {}" .format(x, height))
+    #Printing is possible while the fun is executing
+    #for result in next_heights:
+    #    print(result)
 if __name__ == '__main__':
     main()
