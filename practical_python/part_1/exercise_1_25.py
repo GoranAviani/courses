a33d5f7dfe4539380d0943009ed8dc4a3b228df7
goranaviani <goranaviani@gmail.com>
2020-06-20 15:52:20 +0200

    exercise_1_25.py

--- practical_python/part_1/exercise_1_25.py
+++ practical_python/part_1/exercise_1_25.py
@@ -8,6 +8,14 @@ def main():
     nums = [101, 102, 103]
     items = ['spam', symlist, nums]
     print(items)
+    print(items[0])
+    print(items[0][0])
+    print(items[1])
+    print(items[1][1])
+    print(items[1][1][2])
+    print(items[2])
+    print(items[2][1])
+
 
 if __name__ == '__main__':
     main()
