3ccef0571fbeb1b0868585d3c713f0d83e45cf20
goranaviani <goranaviani@gmail.com>
2020-06-22 08:07:30 +0200

    exercise_2_4.py

--- practical_python/part_2/exercise_2_4.py
+++ practical_python/part_2/exercise_2_4.py
@@ -1,9 +1,24 @@
 """
 Exercise 2.4: A list of tuples
 """
+import csv
+
+def read_portfolio(filename):
+    '''Computes the total cost (shares*price) of a portfolio file'''
+    total_cost = 0.0
+    portfolio = []
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            holding = (row[0], int(row[1]), float(row[2]))
+            portfolio.append(holding)
+    return portfolio
 
 def main():
-    pass
+    portfolio = read_portfolio('files/portfolio.csv')
+    print(portfolio)
 
 if __name__ == '__main__':
     main()
