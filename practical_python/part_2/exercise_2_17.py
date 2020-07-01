0d1b04e11481f5a63394b8f4c9c0a9c432309fe0
goranaviani <goranaviani@gmail.com>
2020-07-01 19:51:27 +0200

    exercise_2_17.py fixup

--- practical_python/part_2/exercise_2_17.py
+++ practical_python/part_2/exercise_2_17.py
@@ -15,3 +15,19 @@ print(prices.items())
 
 pricelist = list(zip(prices.values(),prices.keys()))
 print(pricelist)
+
+print(min(pricelist))
+print(max(pricelist))
+
+print(sorted(pricelist))
+
+
+a = [1, 2, 3, 4]
+b = ['w', 'x', 'y', 'z']
+c = [0.2, 0.4, 0.6, 0.8]
+print(list(zip(a, b, c)))
+
+
+a = [1, 2, 3, 4, 5, 6]
+b = ['x', 'y', 'z']
+print(list(zip(a,b)))
