cd38cc4aa7d0be1644eb19e67fdeba7bad4555a2
goranaviani <goranaviani@gmail.com>
2020-07-01 17:17:38 +0200

    exercise_2_14.py

--- practical_python/part_2/exercise_2_14.py
+++ practical_python/part_2/exercise_2_14.py
@@ -2,8 +2,11 @@
 Exercise 2.14: More sequence operations
 """
 
+def main():
+    data = [4, 9, 1, 25, 16, 100, 49]
+    print(min(data))
+    print(max(data))
+    print(sum(data))
 
-data = [4, 9, 1, 25, 16, 100, 49]
-print(min(data))
-print(max(data))
-print(sum(data))
+if __name__ == '__main__':
+    main()
