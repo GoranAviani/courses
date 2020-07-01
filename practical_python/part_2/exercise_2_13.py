cf28a091d54e51981e12ba3a51ccd6d52da7655d
goranaviani <goranaviani@gmail.com>
2020-07-01 17:16:00 +0200

    exercise_2_13.py

---
+++ practical_python/part_2/exercise_2_13.py
@@ -0,0 +1,16 @@
+"""
+Exercise 2.13: Counting
+"""
+
+
+for n in range(10):            # Count 0 ... 9
+    print(n, end=' ')
+print('\n')
+
+for n in range(10, 0, -1):       # Count 10 ... 1
+    print(n, end='')
+print('\n')
+
+for n in range(0, 10, 2):        # Count 0, 2, ... 8
+    print(n, end=' ')
+print('\n')
