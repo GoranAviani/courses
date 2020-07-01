5ab18e33663e49d6ba1e5751242d159da78611c9
goranaviani <goranaviani@gmail.com>
2020-07-01 19:51:53 +0200

    exercise_2_17.py fixup

--- practical_python/part_2/exercise_2_17.py
+++ practical_python/part_2/exercise_2_17.py
@@ -1,33 +1,36 @@
 """
 Exercise 2.17: Inverting a dictionary
 """
-
-prices = {
-        'GOOG' : 490.1,
-        'AA' : 23.45,
-        'IBM' : 91.1,
-        'MSFT' : 34.23
-    }
-
-
-print(prices.items())
+def main():
+    prices = {
+            'GOOG' : 490.1,
+            'AA' : 23.45,
+            'IBM' : 91.1,
+            'MSFT' : 34.23
+        }
 
 
-pricelist = list(zip(prices.values(),prices.keys()))
-print(pricelist)
-
-print(min(pricelist))
-print(max(pricelist))
-
-print(sorted(pricelist))
+    print(prices.items())
 
 
-a = [1, 2, 3, 4]
-b = ['w', 'x', 'y', 'z']
-c = [0.2, 0.4, 0.6, 0.8]
-print(list(zip(a, b, c)))
+    pricelist = list(zip(prices.values(),prices.keys()))
+    print(pricelist)
+
+    print(min(pricelist))
+    print(max(pricelist))
+
+    print(sorted(pricelist))
 
 
-a = [1, 2, 3, 4, 5, 6]
-b = ['x', 'y', 'z']
-print(list(zip(a,b)))
+    a = [1, 2, 3, 4]
+    b = ['w', 'x', 'y', 'z']
+    c = [0.2, 0.4, 0.6, 0.8]
+    print(list(zip(a, b, c)))
+
+
+    a = [1, 2, 3, 4, 5, 6]
+    b = ['x', 'y', 'z']
+    print(list(zip(a,b)))
+
+if __name__ == '__main__':
+    main()
