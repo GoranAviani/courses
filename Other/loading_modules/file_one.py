ba9b6238e42c7d0bb90968c6c985777a60df0773
goranaviani <goranaviani@gmail.com>
2020-07-04 11:05:50 +0200

    Displaying __name__ variables

--- Other/loading_modules/file_one.py
+++ Other/loading_modules/file_one.py
@@ -1,12 +1,12 @@
-# Python program to execute
-# main directly
-print("Always executed")
+# Python module to execute
 
+
+print("File one __name__ is set to: {}" .format(__name__))
 
 def function_one():
     print("Function one is executed")
 
-print("File one __name__ is set to: {}" .format(__name__))
+
 if __name__ == "__main__":
     print("Executed when invoked directly")
 else:
