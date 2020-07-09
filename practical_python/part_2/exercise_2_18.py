08958c96efb5f6f21fb91c1065a8587848c85111
goranaviani <goranaviani@gmail.com>
2020-07-09 21:34:14 +0200

    reading portfolio

---
+++ practical_python/part_2/exercise_2_18.py
@@ -0,0 +1,26 @@
+"""
+Exercise 2.18: Tabulating with Counters
+"""
+import csv
+
+def read_portfolio(filename):
+    '''Computes the total cost (shares*price) of a portfolio file'''
+    total_cost = 0.0
+    portfolio = []
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            holding = {'name': row[0], 'shares': int(row[1]), 'price': float(row[2])}
+            portfolio.append(holding)
+    return portfolio
+
+def main():
+    portfolio = read_portfolio('files/portfolio.csv')
+
+
+
+if __name__ == '__main__':
+    main()
+
