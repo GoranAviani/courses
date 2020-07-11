e9986a4be416a56368a1172e05f0edcd748c7664
goranaviani <goranaviani@gmail.com>
2020-07-11 21:59:22 +0200

    main2

--- Other/article_code/exploring_requests/main2.py
+++ Other/article_code/exploring_requests/main2.py
@@ -3,7 +3,7 @@ import requests
 def main():
 
     result = requests.get("http://api.open-notify.org/astros.json")
-    print(result)
+    print(result.status_code)
 
 if __name__ == '__main__':
     main()
