9cb5517a245a281aaccf1e5832bc8cdebb40e0b6
goranaviani <goranaviani@gmail.com>
2020-07-13 20:41:44 +0200

    add extra check

--- Other/checkio/acceptable_password_length.py
+++ Other/checkio/acceptable_password_length.py
@@ -6,11 +6,22 @@ The verification condition is:
     the length should be bigger than 6.
 """
 
-def main(password):
-    if len(password) > 6:
-        return True
-    else:
-        return False
+def main(password = "muchlonger5"):
+    digs = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    has_d_in_password = False
+
+    for d in digs:
+        if d in password:
+            has_d_in_password = True
+            break
+
+    if has_d_in_password == True:
+        if len(password) > 6:
+            return True
+        else:
+            return False
+
+    return False
 
 if __name__ == '__main__':
     main()
