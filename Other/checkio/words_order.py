e61b7602f18bd33f29f97884a74cf2f95636d9bf
goranaviani <goranaviani@gmail.com>
2020-09-07 15:05:14 +0200

    words_order.py passing tests, adding new fun. Quickfix - added rule: checking if all wanted words are actualy in te text

--- Other/checkio/words_order.py
+++ Other/checkio/words_order.py
@@ -30,6 +30,12 @@ words_order('hi world im here', ['wo', 'rld']) == False
 words_order('', ['world', 'here']) == False
 '''
 
+def check_word_availability(text_list, words):
+    for word in words:
+        if word not in text_list:
+            return False
+    return True
+
 def check_duplicates(words: dict):
     for word in words:
         if words.count(word) > 1:
@@ -43,6 +49,10 @@ def words_order(text: str, words: list) -> bool:
         return False
 
     text_list = text.split()
+    words_available = check_word_availability(text_list, words)
+    if not words_available:
+        return False
+
     result = {}
     for word in words:
         counter = 0
@@ -71,20 +81,20 @@ def check_results(result : dict):
 
 if __name__ == '__main__':
     print("Example:")
-    print(words_order('hi world im here', ['world', 'here']))
+
 
 
 
     # These "asserts" are used for self-checking and not for an auto-testing
-    assert words_order('hi world im here', ['world', 'here']) == True
-    assert words_order('hi world im here', ['here', 'world']) == False
-    assert words_order('hi world im here', ['world']) == True
-    assert words_order('hi world im here',['world', 'here', 'hi']) == False
-    assert words_order('hi world im here',['world', 'im', 'here']) == True
-    assert words_order('hi world im here',['world', 'hi', 'here']) == False
-    assert words_order('hi world im here', ['world', 'world']) == False
-    #assert words_order('hi world im here',['country', 'world']) == False
-    #assert words_order('hi world im here', ['wo', 'rld']) == False
+    #assert words_order('hi world im here', ['world', 'here']) == True
+    #assert words_order('hi world im here', ['here', 'world']) == False
+    #assert words_order('hi world im here', ['world']) == True
+    #assert words_order('hi world im here',['world', 'here', 'hi']) == False
+    #assert words_order('hi world im here',['world', 'im', 'here']) == True
+    #assert words_order('hi world im here',['world', 'hi', 'here']) == False
+    #assert words_order('hi world im here', ['world', 'world']) == False
+    assert words_order('hi world im here',['country', 'world']) == False
+    assert words_order('hi world im here', ['wo', 'rld']) == False
     #assert words_order('', ['world', 'here']) == False
     print("Coding complete? Click 'Check' to earn cool rewards!")
 
