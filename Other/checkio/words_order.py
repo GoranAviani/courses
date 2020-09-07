177ae0a74b53cf70efd20ba2ef0eed22b5eb5007
goranaviani <goranaviani@gmail.com>
2020-09-07 15:07:46 +0200

    words_order.py all tests passing

--- Other/checkio/words_order.py
+++ Other/checkio/words_order.py
@@ -86,15 +86,15 @@ if __name__ == '__main__':
 
 
     # These "asserts" are used for self-checking and not for an auto-testing
-    #assert words_order('hi world im here', ['world', 'here']) == True
-    #assert words_order('hi world im here', ['here', 'world']) == False
-    #assert words_order('hi world im here', ['world']) == True
-    #assert words_order('hi world im here',['world', 'here', 'hi']) == False
-    #assert words_order('hi world im here',['world', 'im', 'here']) == True
-    #assert words_order('hi world im here',['world', 'hi', 'here']) == False
-    #assert words_order('hi world im here', ['world', 'world']) == False
+    assert words_order('hi world im here', ['world', 'here']) == True
+    assert words_order('hi world im here', ['here', 'world']) == False
+    assert words_order('hi world im here', ['world']) == True
+    assert words_order('hi world im here',['world', 'here', 'hi']) == False
+    assert words_order('hi world im here',['world', 'im', 'here']) == True
+    assert words_order('hi world im here',['world', 'hi', 'here']) == False
+    assert words_order('hi world im here', ['world', 'world']) == False
     assert words_order('hi world im here',['country', 'world']) == False
     assert words_order('hi world im here', ['wo', 'rld']) == False
-    #assert words_order('', ['world', 'here']) == False
+    assert words_order('', ['world', 'here']) == False
     print("Coding complete? Click 'Check' to earn cool rewards!")
 
