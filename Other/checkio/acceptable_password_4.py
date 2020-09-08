ab35696ca28571aa2e5367c2eb3368f771bf8a46
goranaviani <goranaviani@gmail.com>
2020-09-08 15:57:01 +0200

    acceptable_password_4.py

---
+++ Other/checkio/acceptable_password_4.py
@@ -0,0 +1,24 @@
+"""
+ In this mission you need to create a password verification function.
+
+Those are the verification conditions:
+
+    the length should be bigger than 6;
+    should contain at least one digit, but it cannot consist of just digits;
+    having numbers or containing just numbers does not apply to the password longer than 9.
+
+Input: A string.
+
+Output: A bool.
+
+Example:
+is_acceptable_password('short') == False
+is_acceptable_password('short54') == True
+is_acceptable_password('muchlonger') == True
+is_acceptable_password('ashort') == False
+is_acceptable_password('muchlonger5') == True
+is_acceptable_password('sh5') == False
+is_acceptable_password('1234567') == False
+is_acceptable_password('12345678910') == True
+
+"""
