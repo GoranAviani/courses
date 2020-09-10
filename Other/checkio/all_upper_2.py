e83865a460c5af2d4dbd855157613e83b4a14959
goranaviani <goranaviani@gmail.com>
2020-09-10 16:41:11 +0200

    all_upper_2.py

--- Other/checkio/all_upper_2.py
+++ Other/checkio/all_upper_2.py
@@ -13,13 +13,19 @@ is_all_upper('') == False
 """
 
 def is_all_upper(text: str) -> bool:
-    # your code here
-    return False
+    if not text:
+        return False
+
+    for letter in text:
+        if letter == letter.upper():
+            pass
+        else:
+            return False
+    return True
 
 
 if __name__ == '__main__':
-    print("Example:")
-    print(is_all_upper('ALL UPPER'))
+
 
     # These "asserts" are used for self-checking and not for an auto-testing
     assert is_all_upper('ALL UPPER') == True
