5b9d7c043ff9162c43bf2221211a2e925b745e5a
goranaviani <goranaviani@gmail.com>
2020-09-10 16:54:22 +0200

    all_upper_2.py

--- Other/checkio/all_upper_2.py
+++ Other/checkio/all_upper_2.py
@@ -1,5 +1,6 @@
 """
- Check if a given string has all symbols in upper case. If the string is empty or doesn't have any letter in it - function should return False.
+ Check if a given string has all symbols in upper case.
+ If the string is empty or doesn't have any letter in it - function should return False.
 
 Input: A string.
 
@@ -13,7 +14,7 @@ is_all_upper('') == False
 """
 import re
 def check_for_letter_number(text:str):
-    has_letter_number = re.search('[a-zA-Z0-9]', text)
+    has_letter_number = re.search('[a-zA-Z]', text)
     if not has_letter_number:
         return False
     return True
@@ -27,10 +28,11 @@ def is_all_upper(text: str) -> bool:
         return False
 
     for letter in text:
-        if letter.isupper() or letter in [' ', ',', '-']:
+        if letter.isupper() or letter in [' ', ',', '-'] or letter.isdigit():
             pass
         else:
             return False
+
     return True
 
 
@@ -38,8 +40,11 @@ if __name__ == '__main__':
 
 
     # These "asserts" are used for self-checking and not for an auto-testing
-    #assert is_all_upper('ALL UPPER') == True
-    #assert is_all_upper('all lower') == False
-    #assert is_all_upper('mixed UPPER and lower') == False
+    assert is_all_upper('ALL UPPER') == True
+    assert is_all_upper('all lower') == False
+    assert is_all_upper('mixed UPPER and lower') == False
     assert is_all_upper('   ') == False
+    assert is_all_upper('') == False
+    assert is_all_upper('DIGITS123') == True
+    assert is_all_upper('123') == False
     print("Coding complete? Click 'Check' to earn cool rewards!")
