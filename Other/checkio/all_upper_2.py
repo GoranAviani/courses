fc1522e1b79f0f3bef6df0dfc02718422a835331
goranaviani <goranaviani@gmail.com>
2020-09-10 16:39:54 +0200

    all_upper_2.py

--- Other/checkio/all_upper_2.py
+++ Other/checkio/all_upper_2.py
@@ -11,3 +11,19 @@ is_all_upper('all lower') == False
 is_all_upper('mixed UPPER and lower') == False
 is_all_upper('') == False
 """
+
+def is_all_upper(text: str) -> bool:
+    # your code here
+    return False
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(is_all_upper('ALL UPPER'))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert is_all_upper('ALL UPPER') == True
+    assert is_all_upper('all lower') == False
+    assert is_all_upper('mixed UPPER and lower') == False
+    assert is_all_upper('') == False
+    print("Coding complete? Click 'Check' to earn cool rewards!")
