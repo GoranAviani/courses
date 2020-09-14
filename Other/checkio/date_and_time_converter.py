76f2f611a728dc371ddd965ba0bc04fc4d91c0ec
goranaviani <goranaviani@gmail.com>
2020-09-14 15:57:43 +0200

    date_and_time_converter.py

--- Other/checkio/date_and_time_converter.py
+++ Other/checkio/date_and_time_converter.py
@@ -16,3 +16,18 @@ date_time("21.10.1999 18:01") == "21 October 1999 year 18 hours 1 minute"
 # NB: words "hour" and "minute" are used only when time is 01:mm (1 hour) or hh:01 (1 minute).
 # In other cases it should be used "hours" and "minutes".
 '''
+
+
+def date_time(time: str) -> str:
+    #replace this for solution
+    return time
+
+if __name__ == '__main__':
+    print("Example:")
+    print(date_time('01.01.2000 00:00'))
+
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert date_time("01.01.2000 00:00") == "1 January 2000 year 0 hours 0 minutes", "Millenium"
+    assert date_time("09.05.1945 06:30") == "9 May 1945 year 6 hours 30 minutes", "Victory"
+    assert date_time("20.11.1990 03:55") == "20 November 1990 year 3 hours 55 minutes", "Somebody was born"
+    print("Coding complete? Click 'Check' to earn cool rewards!")
