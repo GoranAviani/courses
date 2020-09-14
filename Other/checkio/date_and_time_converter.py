db3d46456acefafb7f24013d2259fb4eda0d72b5
goranaviani <goranaviani@gmail.com>
2020-09-14 16:17:01 +0200

    date_and_time_converter.py

--- Other/checkio/date_and_time_converter.py
+++ Other/checkio/date_and_time_converter.py
@@ -17,14 +17,49 @@ date_time("21.10.1999 18:01") == "21 October 1999 year 18 hours 1 minute"
 # In other cases it should be used "hours" and "minutes".
 '''
 
+MONTH_NUMBER = {'1': 'January', '2': 'February', '3': 'March', '4': 'April', '5': 'May',
+                '6': 'June', '7': 'July', '8': 'August', '9': 'September', '10': 'October',
+                '11': 'November', '12': 'December'}
 
-def date_time(time: str) -> str:
-    #replace this for solution
-    return time
+def date_time(date_and_time: str) -> str:
+    date_and_time_list = date_and_time.split(' ')
+    date = date_and_time_list[0]
+    time = date_and_time_list[1]
+
+    date = date.split('.')
+    time = time.split(':')
+
+    day = date[0]
+    day = day.replace('0', '')
+    month = date[1]
+    month = month.replace('0', '')
+    year = date[2]
+
+    hour = time[0]
+    minutes = time[1]
+
+    hour = hour.replace('0', '')
+    if not hour:
+        hour = '0'
+    minutes = minutes.replace('0', '')
+    if not minutes:
+        minutes = '0'
+
+    date_result = '{} {} {} year' .format(day, MONTH_NUMBER[month], year)
+    if hour == '1':
+        hour_string = 'hour'
+    else:
+        hour_string = 'hours'
+
+    if minutes == '1':
+        minutes_string = 'minute'
+    else:
+        minutes_string = 'minutes'
+    time_result = '{} {} {} {}' .format(hour, hour_string, minutes, minutes_string)
+
+    return date_result + ' ' + time_result
 
 if __name__ == '__main__':
-    print("Example:")
-    print(date_time('01.01.2000 00:00'))
 
     #These "asserts" using only for self-checking and not necessary for auto-testing
     assert date_time("01.01.2000 00:00") == "1 January 2000 year 0 hours 0 minutes", "Millenium"
