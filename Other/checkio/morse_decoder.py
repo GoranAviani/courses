74ecc8f86fd72e0b2fa50f94bf82eac55eb61830
goranaviani <goranaviani@gmail.com>
2020-09-15 15:44:14 +0200

    morse_decoder.py

--- Other/checkio/morse_decoder.py
+++ Other/checkio/morse_decoder.py
@@ -8,18 +8,47 @@ Input: The secret message.
 Output: The decrypted text.
 '''
 
-MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
-                   'C': '-.-.', 'D': '-..', 'E': '.',
-                   'F': '..-.', 'G': '--.', 'H': '....',
-                   'I': '..', 'J': '.---', 'K': '-.-',
-                   'L': '.-..', 'M': '--', 'N': '-.',
-                   'O': '---', 'P': '.--.', 'Q': '--.-',
-                   'R': '.-.', 'S': '...', 'T': '-',
-                   'U': '..-', 'V': '...-', 'W': '.--',
-                   'X': '-..-', 'Y': '-.--', 'Z': '--..',
-                   '1': '.----', '2': '..---', '3': '...--',
-                   '4': '....-', '5': '.....', '6': '-....',
-                   '7': '--...', '8': '---..', '9': '----.',
-                   '0': '-----', ', ': '--..--', '.': '.-.-.-',
-                   '?': '..--..', '/': '-..-.', '-': '-....-',
-                   '(': '-.--.', ')': '-.--.-', " ": "/"}
+MORSE = {'.-':    'a', '-...':  'b', '-.-.':  'c',
+         '-..':   'd', '.':     'e', '..-.':  'f',
+         '--.':   'g', '....':  'h', '..':    'i',
+         '.---':  'j', '-.-':   'k', '.-..':  'l',
+         '--':    'm', '-.':    'n', '---':   'o',
+         '.--.':  'p', '--.-':  'q', '.-.':   'r',
+         '...':   's', '-':     't', '..-':   'u',
+         '...-':  'v', '.--':   'w', '-..-':  'x',
+         '-.--':  'y', '--..':  'z', '-----': '0',
+         '.----': '1', '..---': '2', '...--': '3',
+         '....-': '4', '.....': '5', '-....': '6',
+         '--...': '7', '---..': '8', '----.': '9'
+        }
+
+def morse_decoder(code):
+    #replace this for solution
+    code_saved_in_lists = []
+    words= code.split('   ')
+    for word in words:
+        word = word.split(' ')
+        code_saved_in_lists.append(word)
+
+
+    test = code_saved_in_lists
+    test1 = code_saved_in_lists
+    result = ''
+    for word_code in code_saved_in_lists:
+        result_word = ''
+        for letter_code in word_code:
+            result_word += MORSE[letter_code]
+        result += result_word
+        if code_saved_in_lists.index(word_code) != len(code_saved_in_lists):
+            result += ' '
+
+    return result
+
+
+if __name__ == '__main__':
+
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert morse_decoder("... --- -- .   - . -..- -") == "Some text"
+    assert morse_decoder("..--- ----- .---- ---..") == "2018"
+    assert morse_decoder(".. -   .-- .- ...   .-   --. --- --- -..   -.. .- -.--") == "It was a good day"
+    print("Coding complete? Click 'Check' to earn cool rewards!")
