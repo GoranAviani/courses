9b41e18a3b4ae4ec1dec012b1b03c54cf5534f42
goranaviani <goranaviani@gmail.com>
2020-09-15 15:34:43 +0200

    morse_decoder.py

--- Other/checkio/morse_decoder.py
+++ Other/checkio/morse_decoder.py
@@ -1,3 +1,13 @@
+'''
+Your task is to decrypt the secret message using the Morse code.
+The message will consist of words with 3 spaces between them and 1 space between each letter of each word.
+If the decrypted text starts with a letter then you'll have to print this letter in uppercase.
+
+example
+Input: The secret message.
+Output: The decrypted text.
+'''
+
 MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
                    'C': '-.-.', 'D': '-..', 'E': '.',
                    'F': '..-.', 'G': '--.', 'H': '....',
