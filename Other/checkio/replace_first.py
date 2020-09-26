dbd94c7d0f03b817d78d5b96f73021d230f249b1
goranaviani <goranaviani@gmail.com>
2020-09-26 19:39:14 +0200

    replace_first.py

---
+++ Other/checkio/replace_first.py
@@ -0,0 +1,17 @@
+"""
+Replace First
+Elementary
+RY English RU
+
+In a given list the first element should become the last one. An empty list or list with only one element should stay the same.
+
+example
+
+Input: List.
+
+Output: Iterable.
+
+Example:
+replace_first([1, 2, 3, 4]) == [2, 3, 4, 1]
+replace_first([1]) == [1]
+"""
