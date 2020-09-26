dd8dc0776b7263e9e31c33fb60a4e769abcde5d8
goranaviani <goranaviani@gmail.com>
2020-09-26 19:36:31 +0200

    all_upper_I.py

---
+++ Other/checkio/all_upper_I.py
@@ -0,0 +1,18 @@
+"""
+ All Upper I All Upper I
+Elementary
+RU English
+
+Check if a given string has all symbols in upper case. If the string is empty or doesn't have any letter in it - function should return True.
+
+Input: A string.
+
+Output: a boolean.
+
+Example:
+is_all_upper('ALL UPPER') == True
+is_all_upper('all lower') == False
+is_all_upper('mixed UPPER and lower') == False
+is_all_upper('') == True
+
+"""
