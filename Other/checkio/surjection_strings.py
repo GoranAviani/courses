3677f3fda02534f883459a1e50072088cddc98b0
goranaviani <goranaviani@gmail.com>
2020-10-07 16:57:22 +0200

    surjection_strings.py

--- Other/checkio/surjection_strings.py
+++ Other/checkio/surjection_strings.py
@@ -18,5 +18,21 @@ isometric_strings('foo', 'bar') == False
 2
 
 Precondition:
-both strings are the same size 
+both strings are the same size
 """
+
+def isometric_strings(str1: str, str2: str) -> bool:
+    # your code here
+    return None
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(isometric_strings('add', 'egg'))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert isometric_strings('add', 'egg') == True
+    assert isometric_strings('foo', 'bar') == False
+    assert isometric_strings('', '') == True
+    assert isometric_strings('all', 'all') == True
+    print("Coding complete? Click 'Check' to earn cool rewards!")
