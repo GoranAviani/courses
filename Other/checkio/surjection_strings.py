31663d53f635c5e3a4f19b876004b1f7cfbf55f5
goranaviani <goranaviani@gmail.com>
2020-10-07 17:02:19 +0200

    surjection_strings.py

--- Other/checkio/surjection_strings.py
+++ Other/checkio/surjection_strings.py
@@ -46,7 +46,7 @@ if __name__ == '__main__':
     #print(isometric_strings('add', 'egg'))
 
     # These "asserts" are used for self-checking and not for an auto-testing
-    #assert isometric_strings('add', 'egg') == True
+    assert isometric_strings('add', 'egg') == True
     assert isometric_strings('foo', 'bar') == False
     assert isometric_strings('', '') == True
     assert isometric_strings('all', 'all') == True
