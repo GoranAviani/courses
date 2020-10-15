eccefd139489c51ec3c1bd49d19147fce1949254
goranaviani <goranaviani@gmail.com>
2020-10-15 16:34:41 +0200

    35

--- w3resource/Python/dicts/37_ Write_a_Python_program_to_replace_dictionary_values_with_their_average.py
+++ w3resource/Python/dicts/37_ Write_a_Python_program_to_replace_dictionary_values_with_their_average.py
@@ -1,3 +1,8 @@
 """
 37. Write a Python program to replace dictionary values with their average
 """
+student_details= [
+  {'id' : 1, 'subject' : 'math', 'V' : 70, 'VI' : 82},
+  {'id' : 2, 'subject' : 'math', 'V' : 73, 'VI' : 74},
+  {'id' : 3, 'subject' : 'math', 'V' : 75, 'VI' : 86}
+]
