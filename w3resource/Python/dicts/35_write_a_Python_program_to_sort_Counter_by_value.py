75ed7fc1256e4447fa2c3d8be001c12c0ac8e6c9
goranaviani <goranaviani@gmail.com>
2020-10-15 16:31:10 +0200

    35

--- w3resource/Python/dicts/35_write_a_Python_program_to_sort_Counter_by_value.py
+++ w3resource/Python/dicts/35_write_a_Python_program_to_sort_Counter_by_value.py
@@ -3,3 +3,7 @@
 Sample data : {'Math':81, 'Physics':83, 'Chemistry':87}
 Expected data: [('Chemistry', 87), ('Physics', 83), ('Math', 81)]
 """
+
+from collections import Counter
+x = Counter({'Math':81, 'Physics':83, 'Chemistry':87}).most_common()
+print(x)
