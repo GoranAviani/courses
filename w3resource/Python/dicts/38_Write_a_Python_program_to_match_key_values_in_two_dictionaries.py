8c26d6a49271ec121ea6df4ce6a243c9f75db142
goranaviani <goranaviani@gmail.com>
2020-10-17 17:46:06 +0200

    38

--- w3resource/Python/dicts/38_Write_a_Python_program_to_match_key_values_in_two_dictionaries.py
+++ w3resource/Python/dicts/38_Write_a_Python_program_to_match_key_values_in_two_dictionaries.py
@@ -3,3 +3,14 @@
 Sample dictionary: {'key1': 1, 'key2': 3, 'key3': 2}, {'key1': 1, 'key2': 2}
 Expected output: key1: 1 is present in both x and y
 """
+
+list1 = {'key1': 1, 'key2': 3, 'key3': 2}
+list2 = {'key1': 1, 'key2': 2}
+
+result = []
+for key1, value1 in list1.items():
+    for key2, value2 in list2.items():
+        if key1 == key2 and value1 == value2:
+            result.append(key1)
+
+print(result)
