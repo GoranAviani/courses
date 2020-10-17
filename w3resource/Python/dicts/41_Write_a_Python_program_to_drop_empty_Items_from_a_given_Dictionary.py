e5892ae824f5a2d305ceab13d5272fd997c964fe
goranaviani <goranaviani@gmail.com>
2020-10-17 17:49:43 +0200

    41

---
+++ w3resource/Python/dicts/41_Write_a_Python_program_to_drop_empty_Items_from_a_given_Dictionary.py
@@ -0,0 +1,7 @@
+"""
+41. Write a Python program to drop empty Items from a given Dictionary. Go to the editor
+Original Dictionary:
+{'c1': 'Red', 'c2': 'Green', 'c3': None}
+New Dictionary after dropping empty items:
+{'c1': 'Red', 'c2': 'Green'}
+"""
