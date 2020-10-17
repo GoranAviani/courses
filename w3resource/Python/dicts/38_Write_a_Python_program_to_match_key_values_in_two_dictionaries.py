489a26e7bbb81c74930538690e4fd1f7d0cebb98
goranaviani <goranaviani@gmail.com>
2020-10-17 17:43:22 +0200

    38

---
+++ w3resource/Python/dicts/38_Write_a_Python_program_to_match_key_values_in_two_dictionaries.py
@@ -0,0 +1,5 @@
+"""
+38. Write a Python program to match key values in two dictionaries. Go to the editor
+Sample dictionary: {'key1': 1, 'key2': 3, 'key3': 2}, {'key1': 1, 'key2': 2}
+Expected output: key1: 1 is present in both x and y
+"""
