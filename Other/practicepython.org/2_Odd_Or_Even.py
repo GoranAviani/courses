90c79a86dbdc700f1d0d194e929763cfeb0e8751
goranaviani <goranaviani@gmail.com>
2020-10-26 16:05:41 +0100

    2_Odd_Or_Even.py

--- Other/practicepython.org/2_Odd_Or_Even.py
+++ Other/practicepython.org/2_Odd_Or_Even.py
@@ -5,3 +5,9 @@ Extras:
     If the number is a multiple of 4, print out a different message.
     Ask the user for two numbers: one number to check (call it num) and one number to divide by (check). If check divides evenly into num, tell that to the user. If not, print a different appropriate message.
 """
+user_input = input("input number: ")
+user_input = int(user_input)
+if user_input % 2 == 0:
+    print("even")
+else:
+    print("odd")
