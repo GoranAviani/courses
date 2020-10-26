a058713ac285fa7975ea54ac7744fa641b410924
goranaviani <goranaviani@gmail.com>
2020-10-26 15:53:01 +0100

    1_Character_Input.py

--- Other/practicepython.org/1_Character_Input.py
+++ Other/practicepython.org/1_Character_Input.py
@@ -7,12 +7,21 @@ Extras:
     Print out that many copies of the previous message on separate lines. (Hint: the string "\n is the same as pressing the ENTER button)
 """
 
-user_input = input("How old are you?")
-user_input_2 = input("How many times do you want the message to be printed out?")
-user_input = int(user_input)
-user_input_2 = int(user_input_2)
-year = 2020
-year_diff = 100 - user_input
-result = year + year_diff
-for x in range(0, user_input_2):
-    print("You will be 100 years on in {}". format(result))
+def get_user_input():
+    user_input = input("How old are you?")
+    user_input_2 = input("How many times do you want the message to be printed out?")
+    return user_input, user_input_2
+
+def main():
+
+    user_input, user_input_2 = get_user_input()
+    user_input = int(user_input)
+    user_input_2 = int(user_input_2)
+    year = 2020
+    year_diff = 100 - user_input
+    result = year + year_diff
+    for x in range(0, user_input_2):
+        print("You will be 100 years on in {}". format(result))
+
+if __name__ == '__main__':
+    main()
