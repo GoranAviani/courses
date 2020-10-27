c90a633c1fa89761a718d740f390b776482d2492
goranaviani <goranaviani@gmail.com>
2020-10-27 16:38:01 +0100

    update zadatka

--- Other/practicepython.org/3_List_Less_Than_Ten.py
+++ Other/practicepython.org/3_List_Less_Than_Ten.py
@@ -9,6 +9,7 @@ Extras:
     Ask the user for a number and return a list that contains only elements from the original list a that are smaller than that number given by the user.
 """
 user_input = input("Enter a border number: ")
+user_input = int(user_input)
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 new_list = []
 #for x in a:
