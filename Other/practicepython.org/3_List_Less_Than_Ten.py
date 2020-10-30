1e836e44bf64345308827ecbf8b0b25bd2a60808
goranaviani <goranaviani@gmail.com>
2020-10-30 16:12:05 +0100

    3_List_Less_Than_Ten.py

--- Other/practicepython.org/3_List_Less_Than_Ten.py
+++ Other/practicepython.org/3_List_Less_Than_Ten.py
@@ -9,5 +9,7 @@ Extras:
     Ask the user for a number and return a list that contains only elements from the original list a that are smaller than that number given by the user.
 """
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+result = []
 for x in a:
-    print(x)
+    result.append(x)
+print(result)
