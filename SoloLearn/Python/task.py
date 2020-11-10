034ba112c96522b5e6e0c9b5789944a4f21bbb00
goranaviani <goranaviani@gmail.com>
2020-11-10 16:03:47 +0100

    test

--- SoloLearn/Python/task.py
+++ SoloLearn/Python/task.py
@@ -46,3 +46,15 @@ def make_word():
 
 print(list(make_word()))
 
+def decor(func):
+  def wrap():
+    print("============")
+    func()
+    print("============")
+  return wrap
+
+def print_text():
+  print("Hello world!")
+
+decorated = decor(print_text)
+decorated()
