26cac536147222f213c31ef1c01bd379d253c623
GoranAviani <goranaviani@gmail.com>
2020-12-06 18:37:44 +0100

    hello name decorator

--- Other/codingbat/hello_name.py
+++ Other/codingbat/hello_name.py
@@ -1,8 +1,17 @@
+def check_string(func):
+    def check(x):
+        if type(x) is str:
+            return func(x)
+        else:
+            return None
 
+    return check
 
+@check_string
 def hello_name(name):
-    print(name)
+    return name
 
 
 if __name__ == '__main__':
-    hello_name("Goran")
+    result = hello_name(1)
+    print(result)
