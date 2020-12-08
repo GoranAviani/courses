0f1260b1c9463eb10b13a28c2d6c777f80d7f5d3
GoranAviani <goranaviani@gmail.com>
2020-12-08 17:53:07 +0100

    codebat

--- Other/codingbat/sleep_in.py
+++ Other/codingbat/sleep_in.py
@@ -3,7 +3,12 @@ The parameter weekday is True if it is a weekday, and the parameter vacation is 
  We sleep in if it is not a weekday or we're on vacation. Return True if we sleep in.
 """
 def sleep_in(weekday, vacation):
-    pass
+    if weekday and not vacation:
+        return True
+    elif not weekday and not vacation:
+        return True
+    else:
+        return False
 
 
 def main():
