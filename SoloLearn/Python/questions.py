d0a6d166f7c91a134c83645d09beb78f367b099a
goranaviani <goranaviani@gmail.com>
2020-12-20 15:55:38 +0100

    slpy

--- SoloLearn/Python/questions.py
+++ SoloLearn/Python/questions.py
@@ -6,3 +6,12 @@ nums = filter(lambda x: x > 1, nums)
 print(nums)
 print(len(list(nums)))
 
+
+def power(x, y):
+    if y == 0:
+        return 1
+    else:
+        return x * power(x, y - 1)
+
+
+print(power(2, 3))
