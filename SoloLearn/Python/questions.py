2cc5efcd4ab816e03e0f61e8a91949b770827f7b
goranaviani <goranaviani@gmail.com>
2020-12-20 16:04:52 +0100

    slpy

--- SoloLearn/Python/questions.py
+++ SoloLearn/Python/questions.py
@@ -91,3 +91,11 @@ s1 = Student("Amy")
 
 s1.sayHi()
 """
+
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+rect = Rectangle(7, 8)
+print(rect.color)
