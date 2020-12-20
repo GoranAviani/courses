14f703dfe945ff145a66126f08862bac96049ed0
goranaviani <goranaviani@gmail.com>
2020-12-20 16:02:42 +0100

    slpy

--- SoloLearn/Python/questions.py
+++ SoloLearn/Python/questions.py
@@ -51,3 +51,14 @@ class Dog:
 fido = Dog("Fido", "brown")
 print(fido.name)
 fido.bark()
+
+
+class Dog:
+    legs = 4
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+
+fido = Dog("Fido", "brown")
+print(fido.legs)
+print(Dog.legs)
