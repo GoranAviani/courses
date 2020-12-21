393d26002f75493d8f4ada97c019f5feb3a9ddd9
goranaviani <goranaviani@gmail.com>
2020-12-21 14:46:56 +0100

    slpy

--- SoloLearn/Python/questions.py
+++ SoloLearn/Python/questions.py
@@ -158,3 +158,24 @@ class Dog(Wolf):
 
 husky = Dog("Max", "grey")
 husky.bark()
+
+
+class A:
+    def method(self):
+        print("A method")
+
+
+class B(A):
+    def another_method(self):
+        print("B method")
+
+
+class C(B):
+    def third_method(self):
+        print("C method")
+
+
+c = C()
+c.method()
+c.another_method()
+c.third_method()
