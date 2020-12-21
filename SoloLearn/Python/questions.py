f02775c72c48c97ccb2af1608aaf799744080eb5
goranaviani <goranaviani@gmail.com>
2020-12-21 17:20:15 +0100

    slpy

--- SoloLearn/Python/questions.py
+++ SoloLearn/Python/questions.py
@@ -202,3 +202,28 @@ class B(A):
         super().spam()
 
 B().spam()
+
+class Vector2D:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def __add__(self, other):
+        return Vector2D(self.x + other.x, self.y + other.y)
+
+first = Vector2D(5, 7)
+second = Vector2D(3, 9)
+result = first + second
+print(result.x)
+print(result.y)
+
+
+
+
+
+
+
+
+
+
+
+
