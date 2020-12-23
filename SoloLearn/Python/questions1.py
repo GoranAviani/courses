e07e51ea0cb31c5ba7187db22db61cbb9ad91ca3
goranaviani <goranaviani@gmail.com>
2020-12-23 10:35:08 +0100

    Exercise

--- SoloLearn/Python/questions1.py
+++ SoloLearn/Python/questions1.py
@@ -25,3 +25,18 @@ class SpecialString:
 spam = SpecialString("spam")
 hello = SpecialString("Hello world!")
 print(spam / hello)
+
+
+class SpecialString:
+    def __init__(self, cont):
+        self.cont = cont
+
+    def __gt__(self, other):
+        for index in range(len(other.cont)+1):
+            result = other.cont[:index] + ">" + self.cont
+            result += ">" + other.cont[index:]
+            print(result)
+
+spam = SpecialString("spam")
+eggs = SpecialString("eggs")
+spam > eggs
