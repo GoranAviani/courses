8c0ce7f8c290621dbb6e0fd94eb149b38bbf3260
goranaviani <goranaviani@gmail.com>
2020-12-23 10:31:48 +0100

    Exercise

--- SoloLearn/Python/questions1.py
+++ SoloLearn/Python/questions1.py
@@ -12,3 +12,16 @@ second = Vector2D(3, 9)
 result = first + second
 print(result.x)
 print(result.y)
+
+
+class SpecialString:
+    def __init__(self, cont):
+        self.cont = cont
+
+    def __truediv__(self, other):
+        line = "=" * len(other.cont)
+        return "\n".join([self.cont, line, other.cont])
+
+spam = SpecialString("spam")
+hello = SpecialString("Hello world!")
+print(spam / hello)
